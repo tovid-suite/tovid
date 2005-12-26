20a919c149151c1ee6fe127c7ecb92c494a0f4ff
wapcaplet88 <wapcaplet88@gmail.com>
2005-12-26 01:48:14 +0000

    Fleshing out

--- tovid/libtovid/makexml.py
+++ tovid/libtovid/makexml.py
@@ -1,9 +1,45 @@
 #! /usr/bin/env python
 
+"""This module takes a tovid Project, finds all the Disc elements present
+therein, and generates vcdxbuild|dvdauthor XML for the disc (and all its
+menu/video navigational hierarchy), writing the results to the filename
+contained in each Disc element's 'out' option.
+"""
+
 import string, sys
 from libtovid import Project
 
-def gen_dvdauthor_xml(project):
+def vcdimager_xml(project):
+    xml = """<?xml version="1.0"?>
+    <!DOCTYPE videocd PUBLIC "-//GNU/DTD VideoCD//EN"
+      "http://www.gnu.org/software/vcdimager/videocd.dtd">
+    <videocd xmlns="http://www.gnu.org/software/vcdimager/1.0/"
+    """
+    # format == vcd|svcd, version=1.0 (svcd) | 2.0 (vcd)
+    xml += 'class="%s" version="%s">\n' % (format, version)
+
+    # For SVCD:
+    # xml += '<option name="update scan offsets" value="true" />'
+
+    xml += """<info>
+      <album-id>VIDEO_DISC</album-id>
+      <volume-count>1</volume-count>
+      <volume-number>1</volume-number>
+      <restriction>0</restriction>
+    </info>
+    <pvd>
+      <volume-id>VIDEO_DISC</volume-id>
+      <system-id>CD-RTOS CD-BRIDGE</system-id>
+    </pvd>
+    """
+    # segment-items
+    # sequence-items
+    # pbc + selections
+    xml += '</videocd>'
+    return xml
+
+    
+def dvdauthor_xml(project):
     """Write dvdauthor XML files for the given project, returning a list
     of resulting filenames."""
 
@@ -12,39 +48,43 @@ def gen_dvdauthor_xml(project):
         # Write a dvdauthor XML file for each disc
         xmlfile = "%s.xml" % disc.name.replace(' ', '_')
         xmlfiles.append(xmlfile)
-        xml = disc_xml(disc, project)
+        xml = _disc_xml(disc)
         # TODO: write xml to the file
         print xml
 
 
-def disc_xml(disc, project):
+def _disc_xml(disc):
     xml = '<dvdauthor dest="%s">\n' % disc.name.replace(' ', '_')
     xml += '<vmgm>\n'
     # If there's a topmenu, write vmgm-level XML for it
-    if disc.get('topmenu'):
-        topmenu = project.get(disc.get('topmenu'))
+    if len(disc.children) == 1:
+        topmenu = disc.children[0]
         xml += '  <menus>\n'
         xml += '    <video />\n'
         xml += '    <pgc entry="title">\n'
         xml += '      <vob file="%s" />\n' % topmenu.get('out')
         # TODO: Add buttons for each submenu
         # <button>jump titleset N menu;</button>
+        num = 1
+        for submenus in topmenu.children:
+            xml += '      <button>jump titleset %d menu;</button>\n' % num
+            num += 1
         xml += '    </pgc>\n'
 
     xml += '</vmgm>\n'
     # TODO: add titlesets for each submenu
     for menu in topmenu.children:
         xml += '<titleset>\n'
-        xml += menu_xml(menu, project)
+        xml += _menu_xml(menu)
         for video in menu.children:
-            xml += video_xml(video, project)
+            xml += _video_xml(video)
         xml += '</titleset>\n'
         
     xml += '</dvdauthor>\n'
     return xml
 
 
-def menu_xml(menu, project):
+def _menu_xml(menu):
     """Return a string containing dvdauthor XML for the given menu element."""
     xml = '<menus>\n'
     xml += '  <video />\n'
@@ -55,16 +95,18 @@ def menu_xml(menu, project):
     for target in menu.children:
         xml += '    <button>jump title %d;</button>\n' % num
         num += 1
+    xml += '    <button>jump vmgm menu;</button>\n'
     xml += '  </pgc>\n'
     xml += '</menus>\n'
     return xml
 
 
-def video_xml(video, project):
+def _video_xml(video):
     """Return a string containing dvdauthor XML for the given video element."""
 
     xml = '  <pgc>\n'
     xml += '    <vob file="%s" chapters="0" />\n' % video.get('out')
+    xml += '    <post>call menu;</post>\n'
     xml += '  </pgc>\n'
     return xml
 
@@ -77,4 +119,4 @@ if __name__ == '__main__':
     proj = Project.Project()
     proj.load_file(sys.argv[1])
 
-    gen_dvdauthor_xml(proj)
+    dvdauthor_xml(proj)
