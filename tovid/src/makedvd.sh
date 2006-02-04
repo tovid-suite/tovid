9c8949fcfdbdbb205b8a0471c7b4e6f271ea8740
friedrij <pengi.films@gmail.com>
2006-02-04 07:04:30 +0000

    (1) can now properly handle all DVD types, and prompts for a usable DVD when given one that won't work. (2) dvd device is tested for existence, quit on fail

--- tovid/src/makedvd.sh
+++ tovid/src/makedvd.sh
@@ -93,6 +93,29 @@ BURN_SPEED=1
 OUT_DIR="makedvd_out"
 DISC_LABEL=""
 
+# Grab DVD media information
+# Returns nothing, but sets the 'global' variabes MEDIA_INFO, DISC_STATUS, and DISC_TYPE
+probe_media ()
+{
+  # Grad entire media info
+  MEDIA_INFO=`dvd+rw-mediainfo $DVDRW_DEVICE 2>&1`
+ 
+  # Extract the status (either "blank" or "complete")
+  DISC_STATUS=`echo "$MEDIA_INFO" | grep "Disc status" | awk '{ print $3 }'`
+
+  # Extract the disc type
+  DISC_TYPE=`echo "$MEDIA_INFO" | grep "Mounted Media" | awk '{ print $4 }'`
+  if test -z $DISC_TYPE; then DISC_TYPE="no disc"; fi
+
+  # Define usable DVDs
+  HAVE_DVD_RW=`verify $DISC_TYPE set "DVD-RW DVD+RW"`
+  if `verify $DISC_TYPE set "DVD-R DVD+R"` && test "x$DISC_STATUS" = "xblank"
+    then HAVE_BLANK_DVD_R=:
+    else HAVE_BLANK_DVD_R="false"
+  fi
+}
+
+
 # ==========================================================
 # EXECUTION BEGINS HERE
 echo $"$SCRIPTNAME"
@@ -149,7 +172,12 @@ fi
 # Sanity check: Make sure given device is valid (somehow)
 # before creating the image. Give option to proceed anyway?
 # (i.e., could there be any point to proceeding?)
-
+# Here's a simple way: just quit
+if test -b $DVDRW_DEVICE; then :
+else
+  echo "Couldn't find $DVDRW_DEVICE! Stopping here."
+  exit 1
+fi
 
 # TODO:
 # Warn if there isn't enough space to make the image
@@ -240,18 +268,52 @@ fi
     
 # Burn the disc, if requested
 if $DO_BURN; then
+
+    GROWISOFS_VER=`growisofs -version | grep version | awk '{ print $6 }' | sed 's/,//g'`
+
+    # Make sure there is a blank disc to write to
+    probe_media
+
+    if ! $HAVE_DVD_RW && ! $HAVE_BLANK_DVD_R; then
+      echo "Found $DISC_TYPE in $DVDRW_DEVICE. Cannot burn to this disc!"
+      until $HAVE_DVD_RW || $HAVE_BLANK_DVD_R
+      do
+        TIME_SCALE=1
+        # TODO: smarter loop. User feedback (ie "hit any key" [where's the "any" key anyway?]) 
+        # is bad for when makedvd is called from the gui. But, it usually takes 2 cycles for
+        # the dvd player to recognize a new disc. Is there a better interface to it? Waiting
+        # till the drive's LED goes off is good, but how?
+        echo "Found $DISC_STATUS $DISC_TYPE. Please insert a usable DVD into $DVDRW_DEVICE."
+        for COUNTER in 5 4 3 2 1; do
+          printf "Trying again in %2s seconds...\r" `expr $COUNTER \* $TIME_SCALE`
+          sleep ${TIME_SCALE}s
+        done
+        echo
+        echo "Looking for usable media..."
+        probe_media
+      done
+    fi
+
+    echo "Found $DISC_STATUS $DISC_TYPE."
+
+    # DVD-RW need explicit blanking, DVD+RW blanking is done by growisofs automatically
+    if test "x$DISC_TYPE" = "xDVD-RW" && test "x$DISC_STATUS" = "xcomplete"; then 
+      echo "Found $DISC_STATUS $DISC_TYPE in $DVDRW_DEVICE. Blanking..."
+      echo "dvd+rw-format -blank $DVDRW_DEVICE"
+    fi
+
     # If an image was created, burn that
     if $DO_IMAGE; then
-       BURN_CMD="growisofs -dry-run -dvd-compat -speed=$BURN_SPEED -Z $DVDRW_DEVICE=\"$DISC_LABEL.iso\""
+       BURN_CMD="growisofs -use-the-force-luke=dao -dvd-compat -speed=$BURN_SPEED -Z $DVDRW_DEVICE=\"$DISC_LABEL.iso\""
        BURN_METHOD="ISO to DVD"
     # or burn from the DVD directory
     else
-       BURN_CMD="growisofs -dry-run -dvd-compat -speed=$BURN_SPEED -Z $DVDRW_DEVICE -dvd-video -V \"$VOLID\" $OUT_DIR"
+       BURN_CMD="growisofs -use-the-force-luke=dao -dvd-compat -speed=$BURN_SPEED -Z $DVDRW_DEVICE -dvd-video -V \"$VOLID\" $OUT_DIR"
        BURN_METHOD="DVD directly"
     fi
     echo $SEPARATOR
-    echo "Burning $BURN_METHOD with the following command:"
-    echo $BURN_CMD
+    echo "Burning $BURN_METHOD with growisofs $GROWISOFS_VER using the following command:"
+    echo "$BURN_CMD"
     echo $SEPARATOR
     SUCCESS=false
     eval "$BURN_CMD" 2>&1 && SUCCESS=:
