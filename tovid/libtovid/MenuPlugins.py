a6c502fc4408ac79bbfe38cd27ce1218555cc0a9
wapcaplet88 <wapcaplet88@gmail.com>
2006-02-16 11:59:19 +0000

    Fleshing out menu generator

--- tovid/libtovid/MenuPlugins.py
+++ tovid/libtovid/MenuPlugins.py
@@ -1,7 +1,12 @@
 #! /usr/bin/env python
 # MenuPlugins.py
 
+import os
 from Globals import degunk
+
+# TODO: Eliminate this:
+FRAMES=90
+
 
 __doc__=\
 """This module implements several backends for generating MPEG menus
@@ -11,19 +16,262 @@ class MenuPlugin:
     """Base plugin class; all menu-generators inherit from this."""
     def __init__(self, menu):
         self.menu = menu
+        # Alias, for shorter lookups
+        self.get = self.menu.get
+        # List of commands to be executed
+        self.commands = []
+        
+        self.preproc()
+
+    def preproc(self):
+        if self.get('format') == 'dvd':
+            width = 720
+            samprate = 48000
+            if self.get('tvsys') == 'ntsc':
+                height = 480
+            else:
+                height = 576
+        else:
+            width = 352
+            samprate = 44100
+            if self.get('tvsys') == 'ntsc':
+                height = 240
+            else:
+                height = 288
+        
+        self.samprate = samprate
+        # TODO: Proper safe area. Hardcoded for now.
+        self.expand = (width, height)
+        self.scale = (int(width * 0.9), int(height * 0.9))
+        
+    def run(self):
+        for cmd in self.commands:
+            print "MenuPlugin: Running the following command:"
+            print cmd
+            for line in os.popen(cmd, 'r').readlines():
+                print line
 
 
 class TextMenu (MenuPlugin):
-    """Simple menu with selectable text titles"""
+    """Simple menu with selectable text titles. For now, basically a clone
+    of the classic 'makemenu' output."""
     def __init__(self, menu):
         MenuPlugin.__init__(self, menu)
+        # TODO: Store intermediate images in a temp folder
+        self.bg_canvas = 'pymakemenu.bg_canvas.png'
+        self.fg_canvas = 'pymakemenu.fg_canvas.png'
+        self.fg_highlight = 'pymakemenu.fg_highlight.png'
+        self.fg_selection = 'pymakemenu.fg_selection.png'
+        
+        print "Creating a menu with %s titles" % len(self.get('titles'))
+        self.build_reusables()
+        self.draw_background_canvas()
+        self.draw_background_layer()
+        self.draw_highlight_layer()
+        self.draw_selection_layer()
+        self.gen_video()
+        self.gen_audio()
+        self.gen_mpeg()
+        
+
+    def build_reusables(self):
+        """Assemble some re-usable ImageMagick command snippets.
+        
+        For now, just create an ImageMagick draw command for all title text
+        for placing labels and highlights.
+        
+        Corresponds to lines 343-377 of makemenu"""
+        
+        spacing = 30    # Pixel spacing between lines
+        y = 0           # Current y-coordinate
+        y2 = spacing    # Next y-coordinate
+        titlenum = 1        # Title number (for labeling)
+        labels = ''
+        buttons = ''
+        for title in self.get('titles'):
+            print "Adding '%s'" % title
+            # TODO: Escape special characters in title
+            
+            # For VCD, number the titles
+            if self.get('format') == 'vcd':
+                labels += " text 0,%s '%s. %s' " % (y, titlenum, title)
+            # Others use title string alone
+            else:
+                labels += " text 15,%s '%s'" % (y, title)
+                buttons += " text 0,%s '>'" % y
+            
+            # Increment y-coordinates and title number
+            y = y2
+            y2 += spacing
+            titlenum += 1
+
+        self.im_labels = labels
+        self.im_buttons = buttons
+
+    def draw_background_canvas(self):
+        """Draw background canvas.
+        Corresp. to lines 400-411 of makemenu."""
+
+        # Generate default blue-black gradient background
+        # TODO: Implement -background
+        cmd = 'convert -size %sx%s ' % self.expand
+        cmd += ' gradient:blue-black '
+        cmd += ' -gravity center -matte '
+        cmd += ' %s' % self.bg_canvas
+        self.commands.append(cmd)
+
+    def draw_background_layer(self):
+        """Draw the background layer for the menu, including static title text.
+        Corresp. to lines 438-447, 471-477 of makemenu."""
+
+        # Draw text titles on a transparent background.
+        cmd = 'convert -size %sx%s ' % self.scale
+        cmd += ' xc:none -antialias -font "%s" ' % self.get('font')
+        cmd += ' -pointsize %s ' % self.get('fontsize')
+        cmd += ' -fill "%s" ' % self.get('textcolor')
+        cmd += ' -stroke black -strokewidth 3 '
+        cmd += ' -draw "gravity %s %s" ' % (self.get('align'), self.im_labels)
+        cmd += ' -stroke none -draw "gravity %s %s" ' % \
+                (self.get('align'), self.im_labels)
+        cmd += ' %s' % self.fg_canvas
+        self.commands.append(cmd)
+
+        # Composite text over background
+        cmd = 'composite -compose Over -gravity center '
+        cmd += ' %s %s ' % (self.fg_canvas, self.bg_canvas)
+        cmd += ' -depth 8 "%s.ppm"' % self.get('out')
+        self.commands.append(cmd)
 
 
+    def draw_highlight_layer(self):
+        """Draw menu highlight layer, suitable for multiplexing.
+        Corresp. to lines 449-458, 479-485 of makemenu."""
+
+        # Create text layer (at safe-area size)
+        cmd = 'convert -size %sx%s ' % self.scale
+        cmd += ' xc:none -antialias -font "%s" ' % self.get('font')
+        cmd += ' -weight bold '
+        cmd += ' -pointsize %s ' % self.get('fontsize')
+        cmd += ' -fill "%s" ' % self.get('highlightcolor')
+        cmd += ' -draw "gravity %s %s" ' % (self.get('align'), self.im_buttons)
+        cmd += ' -type Palette -colors 3 '
+        cmd += ' png8:%s' % self.fg_highlight
+        self.commands.append(cmd)
+        
+        # Pseudo-composite, to expand layer to target size
+        cmd = 'composite -compose Src -gravity center '
+        cmd += ' %s %s ' % (self.fg_highlight, self.bg_canvas)
+        cmd += ' png8:"%s.hi.png"' % self.get('out')
+        self.commands.append(cmd)
+
+
+    def draw_selection_layer(self):
+        """Draw menu selections on a transparent background.
+        Corresp. to lines 460-469, 487-493 of makemenu."""
+
+        # Create text layer (at safe-area size)
+        cmd = 'convert -size %sx%s ' % self.scale
+        cmd += ' xc:none -antialias -font "%s" ' % self.get('font')
+        cmd += ' -weight bold '
+        cmd += ' -pointsize %s ' % self.get('fontsize')
+        cmd += ' -fill "%s" ' % self.get('selectcolor')
+        cmd += ' -draw "gravity %s %s" ' % (self.get('align'), self.im_buttons)
+        cmd += ' -type Palette -colors 3 '
+        cmd += ' png8:%s' % self.fg_selection
+        self.commands.append(cmd)
+
+        # Pseudo-composite, to expand layer to target size
+        cmd = 'composite -compose Src -gravity center '
+        cmd += ' %s %s ' % (self.fg_selection, self.bg_canvas)
+        cmd += ' png8:"%s.sel.png"' % self.get('out')
+        self.commands.append(cmd)
+
+
+    def gen_video(self):
+        """Generate a video stream (mpeg1/2) from the menu background image.
+        Corresp. to lines 495-502 of makemenu."""
+        
+        # ppmtoy4m part
+        cmd = 'ppmtoy4m -S 420mpeg2 '
+        if self.get('tvsys') == 'ntsc':
+            cmd += ' -A 10:11 -F 30000:1001 '
+        else:
+            cmd += ' -A 59:54 -F 25:1 '
+        cmd += ' -n %s ' % FRAMES
+        cmd += ' -r "%s.ppm" ' % self.get('out')
+        # mpeg2enc part
+        cmd += ' | mpeg2enc -a 2 '
+        # PAL/NTSC
+        if self.get('tvsys') == 'ntsc':
+            cmd += ' -F 4 -n n '
+        else:
+            cmd += ' -F 3 -n p '
+        # Use correct format flags and filename extension
+        if self.get('format') == 'vcd':
+            self.vstream = '%s.m1v' % self.get('out')
+            cmd += ' -f 1 '
+        else:
+            self.vstream = '%s.m2v' % self.get('out')
+            if self.get('format') == 'dvd':
+                cmd += ' -f 8 '
+            elif self.get('format') == 'svcd':
+                cmd += ' -f 4 '
+        cmd += ' -o "%s" ' % self.vstream
+        self.commands.append(cmd)
+
+
+    def gen_audio(self):
+        """Generate an audio stream (mp2/ac3) from the given audio file
+        (or generate silence instead).
+        Corresp. to lines 413-430, 504-518 of makemenu."""
+        # If audio file was provided, create .wav of it
+        if self.get('audio'):
+            cmd = 'sox "%s" ' % self.get('audio')
+            cmd += ' -r %s ' % self.samprate
+            cmd += ' -w "%s.wav" ' % self.get('out')
+        # Otherwise, generate 4-second silence
+        else:
+            cmd = 'cat /dev/zero | sox -t raw -c 2 '
+            cmd += ' -r %s -w -s - ' % self.samprate
+            cmd += ' -t wav "%s.wav" trim 0 4 ' % self.get('out')
+        self.commands.append(cmd)
+        
+        # mp2 for (S)VCD
+        if self.get('format') in ['vcd', 'svcd']:
+            self.astream = '%s.mp2' % self.get('out')
+            cmd = 'cat "%s.wav" | mp2enc ' % self.get('out')
+            cmd += ' -r %s -s ' % self.samprate
+        # ac3 for all others
+        else:
+            self.astream = '%s.ac3' % self.get('out')
+            cmd = 'ffmpeg -i "%s.wav" ' % self.get('out')
+            cmd += ' -ab 224 -ar %s ' % self.samprate
+            cmd += ' -ac 2 -acodec ac3 -y '
+        cmd += ' "%s" ' % self.astream
+        self.commands.append(cmd)
+
+
+    def gen_mpeg(self):
+        """Multiplex audio and video streams to create an mpeg.
+        Corresp. to lines 520-526 of makemenu."""
+        
+        cmd = 'mplex -o "%s.temp.mpg" ' % self.get('out')
+        # Format flags
+        if self.get('format') == 'vcd':
+            cmd += ' -f 1 '
+        else:
+            # Variable bitrate
+            cmd += ' -V '
+            if self.get('format') == 'dvd':
+                cmd += ' -f 8 '
+            elif self.get('format') == 'svcd':
+                cmd += ' -f 4 '
+        cmd += ' "%s" "%s" ' % (self.astream, self.vstream)
+        self.commands.append(cmd)
 
 # ===========================================================
 # Supporting variables and classes for thumbnail menu
 
-FRAMES=90
 
 # List of top-left coordinates and size for default menu
 # thumbnails (for now, a 4x3 grid)
