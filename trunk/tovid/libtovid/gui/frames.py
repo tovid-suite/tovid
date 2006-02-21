e1b3d7e83c9a3575b615b65d0ca632cd9b4914c4
bigdaddydsp <bigdaddydsp@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-02-21 05:22:58 +0000

    Split up tovidgui into python modules in the libtovid/gui package.

---
+++ trunk/tovid/libtovid/gui/frames.py
@@ -0,0 +1,335 @@
+# ###################################################################
+# ###################################################################
+#
+#
+#                             FRAMES
+#
+#
+# ###################################################################
+# ###################################################################
+import os, wx
+from libtovid.gui.configs import TovidConfig
+from libtovid.gui.constants import *
+from libtovid import Project
+from libtovid.gui.panels import AuthorFilesTaskPanel, GuidePanel
+from libtovid.gui.util import _
+
+__all__ = ["MainFrame", "MiniEditorFrame"]
+# ===================================================================
+#
+# CLASS DEFINITION
+# Main tovid GUI frame. Contains and manages all sub-panels.
+#
+# ===================================================================
+class MainFrame(wx.Frame):
+    def __init__(self, parent, id, title):
+        wx.Frame.__init__(self, parent, id , title, wx.DefaultPosition,
+            (800, 600), wx.DEFAULT_FRAME_STYLE | wx.NO_FULL_REPAINT_ON_RESIZE)
+
+        self.SetIcon(wx.Icon("icons/tovid_icon_32.png", wx.BITMAP_TYPE_PNG))
+
+        # Global configuration
+        self.curConfig = TovidConfig()
+
+        self.dirname = os.getcwd()
+
+        # Menu bar
+        self.menubar = wx.MenuBar()
+
+        # File menu
+        self.menuFile = wx.Menu()
+        #self.menuFile.Append(ID_MENU_FILE_PREFS, "&Preferences",
+        #    "Configuration settings for tovid GUI")
+        #self.menuFile.AppendSeparator()
+        self.menuFile.Append(ID_MENU_FILE_OPEN, "&Open project",
+                "Open an existing TDL text file (EXPERIMENTAL)")
+        self.menuFile.Append(ID_MENU_FILE_SAVE, "&Save project",
+                "Save this project as a TDL text file (EXPERIMENTAL)")
+        self.menuFile.AppendSeparator()
+        self.menuFile.Append(ID_MENU_FILE_EXIT, "E&xit",
+                "Exit tovid GUI")
+
+        # Help menu
+        self.menuHelp = wx.Menu()
+        self.menuHelp.Append(ID_MENU_HELP_ABOUT, "&About",
+            "Information about this program")
+
+        # View menu
+        self.menuView = wx.Menu()
+        # Toggle options
+        self.menuViewShowGuide = wx.MenuItem(self.menuView, ID_MENU_VIEW_SHOWGUIDE,
+            "Show &guide", "Show/hide the tovid guide panel", wx.ITEM_CHECK)
+        self.menuViewShowTooltips = wx.MenuItem(self.menuView, ID_MENU_VIEW_SHOWTOOLTIPS,
+            "Show &tooltips", "Show/hide tooltips in the GUI", wx.ITEM_CHECK)
+        # Add items to View menu
+        self.menuView.AppendItem(self.menuViewShowGuide)
+        self.menuView.AppendItem(self.menuViewShowTooltips)
+        self.menuViewShowGuide.Check(False)
+        self.menuViewShowTooltips.Check(True)
+
+        #self.menuLang = wx.Menu()
+        #self.menuLang.Append(ID_MENU_LANG_EN, "&English")
+        #self.menuLang.Append(ID_MENU_LANG_DE, "&Deutsch")
+        # Append language menu as a submenu of View
+        #self.menuView.AppendMenu(ID_MENU_LANG, "Language", self.menuLang)
+
+        
+        # Menu events
+        #wx.EVT_MENU(self, ID_MENU_FILE_PREFS, self.OnFilePrefs)
+        wx.EVT_MENU(self, ID_MENU_FILE_SAVE, self.OnFileSave)
+        wx.EVT_MENU(self, ID_MENU_FILE_OPEN, self.OnFileOpen)
+        wx.EVT_MENU(self, ID_MENU_FILE_EXIT, self.OnExit)
+        wx.EVT_MENU(self, ID_MENU_VIEW_SHOWGUIDE, self.OnShowGuide)
+        wx.EVT_MENU(self, ID_MENU_VIEW_SHOWTOOLTIPS, self.OnShowTooltips)
+        wx.EVT_MENU(self, ID_MENU_HELP_ABOUT, self.OnAbout)
+        wx.EVT_MENU(self, ID_MENU_LANG_EN, self.OnLang)
+        wx.EVT_MENU(self, ID_MENU_LANG_DE, self.OnLang)
+
+        # Build menubar
+        self.menubar.Append(self.menuFile, "&File")
+        self.menubar.Append(self.menuView, "&View")
+        self.menubar.Append(self.menuHelp, "&Help")
+        self.SetMenuBar(self.menubar)
+
+        # Toolbar
+        #self.toolbar = self.CreateToolBar(wx.TB_HORIZONTAL)
+        #self.toolbar.AddControl(wx.ContextHelpButton(self.toolbar))
+        #self.toolbar.Realize()
+
+        # Statusbar
+        self.curConfig.statusBar = self.CreateStatusBar();
+        self.Show(True)
+
+        # Guide panel
+        self.panGuide = GuidePanel(self, wx.ID_ANY)
+        self.panGuide.Hide()
+        # Store guide panel reference in global config
+        self.curConfig.panGuide = self.panGuide
+        
+        # Task panels. AuthorFiles is currently the only working task.
+        self.panAuthorFiles = AuthorFilesTaskPanel(self, wx.ID_ANY)
+
+        # Task sizer. Holds current 3-step task
+        self.sizTask = wx.BoxSizer(wx.VERTICAL)
+        self.sizTask.Add(self.panAuthorFiles, 1, wx.EXPAND)
+
+        # Main sizer. Holds task (layout, encoding) panel and Guide panel
+        self.sizMain = wx.BoxSizer(wx.HORIZONTAL)
+        #self.sizMain.Add(self.panGuide, 2, wx.EXPAND | wx.ALL, 8)
+        #self.sizMain.Add(wx.StaticLine(self, wx.ID_ANY, style = wx.LI_VERTICAL),
+        #    0, wx.EXPAND)
+        self.sizMain.Add(self.sizTask, 5, wx.EXPAND)
+        self.SetSizer(self.sizMain)
+
+
+    def OnFileSave(self, evt):
+        """Save the current project to a TDL file."""
+        outFileDialog = wx.FileDialog(self, _("Select a save location"),
+            self.dirname, "", "*.tdl", wx.SAVE)
+        if outFileDialog.ShowModal() == wx.ID_OK:
+            # Remember current directory
+            self.dirname = outFileDialog.GetDirectory()
+            # Open a file for writing
+            outFile = open(outFileDialog.GetPath(), 'w')
+            
+            elements = self.panAuthorFiles.GetElements()
+            for element in elements:
+                outFile.write(element.tdl_string())
+
+            outFile.close()
+    
+
+    def OnFileOpen(self, evt):
+        inFileDialog = wx.FileDialog(self, _("Choose a TDL file to open"),
+            self.dirname, "", "*.tdl", wx.OPEN)
+        if inFileDialog.ShowModal() == wx.ID_OK:
+            self.dirname = inFileDialog.GetDirectory()
+            proj = Project.Project()
+            proj.load_file(inFileDialog.GetPath())
+            self.panAuthorFiles.SetElements(proj.topitems)
+
+        inFileDialog.Destroy()
+
+        
+    def OnExit(self, evt):
+        """Exit the GUI and close all windows."""
+        self.Close(True)
+
+
+    def OnShowGuide(self, evt):
+        """Show or hide the guide panel."""
+        if evt.IsChecked():
+            self.sizMain.Prepend(self.panGuide, 2, wx.EXPAND | wx.ALL, 8)
+            self.panGuide.Show()
+            self.sizMain.Layout()
+        else:
+            self.sizMain.Remove(0)
+            self.panGuide.Hide()
+            self.sizMain.Layout()
+
+    def OnShowTooltips(self, evt):
+        """Show or hide GUI tooltips."""
+        if evt.IsChecked():
+            # Enable tooltips globally
+            wx.ToolTip_Enable(True)
+        else:
+            # Disable tooltips globally
+            wx.ToolTip_Enable(False)
+        
+    def OnAbout(self, evt):
+        """Display a dialog showing information about tovidgui."""
+        strAbout = "You are using the tovid GUI, version 0.24,\n" \
+          "part of the tovid video disc authoring suite.\n\n" \
+          "For more information and documentation, please\n" \
+          "visit the tovid web site:\n\n" \
+          "http://tovid.org/"
+        dlgAbout = wx.MessageDialog(self, strAbout, "About tovid GUI", wx.OK)
+        dlgAbout.ShowModal()
+
+
+    def OnLang(self, evt):
+        """Change GUI to selected language."""
+        if evt.GetId() == ID_MENU_LANG_EN:
+            self.curConfig.UseLanguage('en')
+            
+        elif evt.GetId() == ID_MENU_LANG_DE:
+            self.curConfig.UseLanguage('de')
+        
+
+    # ==========================================================
+    # Open preferences window and set configuration
+    # ==========================================================
+    #def OnFilePrefs(self, evt):
+    #    dlgPrefs = PreferencesDialog(self, wx.ID_ANY)
+    #    dlgPrefs.ShowModal()
+    #    # Set the output directory in the PreEncoding panel
+    #    self.panEncoding.SetOutputDirectory(self.curConfig.strOutputDirectory)
+          
+# ===================================================================
+# End MainFrame
+# ===================================================================
+
+# ===================================================================
+#
+# CLASS DEFINITION
+# Simple text editor (for editing configuration files) in a frame
+#
+# ===================================================================
+class MiniEditorFrame(wx.Frame):
+    # ==========================================================
+    # Initialize MiniEditorFrame
+    # Takes same arguments as a frame, in addition to a filename
+    # ==========================================================
+    def __init__(self, parent, id, filename):
+        wx.Frame.__init__(self, parent, id, "%s (MiniEditor)" % filename, wx.DefaultPosition,
+            (500, 400), wx.DEFAULT_FRAME_STYLE | wx.NO_FULL_REPAINT_ON_RESIZE)
+
+        # Current directory is current dirname
+        self.dirname = os.getcwd()
+
+        # Center dialog
+        self.Centre()
+
+        # Menu bar
+        self.menubar = wx.MenuBar()
+        
+        # File menu
+        self.menuFile = wx.Menu()
+        self.menuFile.Append(ID_MENU_FILE_NEW, "&New", "Start a new file")
+        self.menuFile.AppendSeparator()
+        self.menuFile.Append(ID_MENU_FILE_OPEN, "&Open", "Open a new file")
+        self.menuFile.Append(ID_MENU_FILE_SAVE, "&Save", "Save current file")
+        self.menuFile.AppendSeparator()
+        self.menuFile.Append(ID_MENU_FILE_EXIT, "E&xit", "Exit mini editor")
+        
+        # Menu events
+        wx.EVT_MENU(self, ID_MENU_FILE_NEW, self.OnNew)
+        wx.EVT_MENU(self, ID_MENU_FILE_OPEN, self.OnOpen)
+        wx.EVT_MENU(self, ID_MENU_FILE_SAVE, self.OnSave)
+        wx.EVT_MENU(self, ID_MENU_FILE_EXIT, self.OnExit)
+
+        # Build menubar
+        self.menubar.Append(self.menuFile, "&File")
+        self.SetMenuBar(self.menubar)
+
+        # Editor window
+        self.editWindow = wx.Editor(self, wx.ID_ANY, style = wx.SUNKEN_BORDER)
+        self.sizMain = wx.BoxSizer(wx.VERTICAL)
+        self.sizMain.Add(self.editWindow, 1, wx.EXPAND | wx.ALL, 6)
+
+        # Open the given filename and load its text into the editor window
+        self.OpenFile(filename)
+
+        self.SetSizer(self.sizMain)
+
+    # Create a new file
+    def OnNew(self, evt):
+        # Just empty edit window and set filename to null
+        self.editWindow.SetText([""])
+        self.currentFilename = ""
+        # Set title to indicate that a new file is being edited
+        self.SetTitle("Untitled file (MiniEditor)")
+
+    def OnOpen(self, evt):
+        inFileDialog = wx.FileDialog(self, _("Choose a file to open"), self.dirname, "",
+            "*.*", wx.OPEN)
+        if inFileDialog.ShowModal() == wx.ID_OK:
+            self.dirname = inFileDialog.GetDirectory()
+            # Open the file
+            self.OpenFile(inFileDialog.GetPath())
+
+        inFileDialog.Destroy()
+
+    def OnSave(self, evt):
+        # Save current file. If currentFilename is set, just overwrite existing
+        if self.currentFilename:
+            # Save the current filename (prompt for overwrite)
+            confirmDialog = wx.MessageDialog(self, _("Overwrite the existing file?"),
+                _("Confirm overwrite"), wx.YES_NO)
+            # If not overwriting, return now
+            if confirmDialog.ShowModal() == wx.ID_NO:
+                return
+
+        # currentFilename is empty; prompt for a filename
+        else:
+            outFileDialog = wx.FileDialog(self, _("Choose a filename to save"), self.dirname,
+                "", "*.*", wx.SAVE)
+            if outFileDialog.ShowModal() == wx.ID_OK:
+                self.dirname = outFileDialog.GetDirectory()
+                self.currentFilename = outFileDialog.GetPath()
+
+        outFile = open(self.currentFilename, "w")
+        for line in self.editWindow.GetText():
+            outFile.write("%s\n" % line)
+        outFile.close()
+
+        # Update title bar
+        self.SetTitle("%s (MiniEditor)" % self.currentFilename)
+
+    def OnExit(self, evt):
+        self.Close(True)
+
+    # Open the given filename
+    def OpenFile(self, filename):
+        # Save the filename locally
+        self.currentFilename = filename
+
+        # Create the file input stream
+        inFile = open(filename, 'r')
+        buffer = inFile.readline()
+        strEditText = []
+        # Read from the file until EOF
+        while buffer:
+            buffer = inFile.readline()
+            # Append file's text to a string
+            strEditText.append(buffer)
+
+        # Fill the editor window with the text buffer
+        self.editWindow.SetText(strEditText)
+        inFile.close()
+
+        # Update titlebar text
+        self.SetTitle("%s (MiniEditor)" % self.currentFilename)
+# ===================================================================
+# End MiniEditorFrame
+# ===================================================================
