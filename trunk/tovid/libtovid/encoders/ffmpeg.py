3759113a0045ad401842255f9fe4862b7b250e71
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-04-13 02:25:20 +0000

    Still more

--- trunk/tovid/libtovid/encoders/ffmpeg.py
+++ trunk/tovid/libtovid/encoders/ffmpeg.py
@@ -5,38 +5,39 @@ __all__ = ['encode']
 
 import logging
 
-from libtovid.cli import Command
+from libtovid.cli import Script
+
+log = logging.getLogger('ffmpeg.py')
 
 def encode(infile, options):
     """Encode infile (a MultimediaFile) with ffmpeg, using the given options."""
-    cmd = Command('ffmpeg')
-    cmd.append('-i "%s"' % infile.filename)
+    cmd = 'ffmpeg'
+    cmd += ' -i "%s"' % infile.filename
     if options['format'] in ['vcd', 'svcd', 'dvd']:
-        cmd.append('-tvstd %s' % options['tvsys'])
-        cmd.append('-target %s-%s' % (options['format'], options['tvsys']))
-    cmd.append('-r %g' % options['fps'])
-    cmd.append('-ar %s' % options['samprate'])
+        cmd += ' -tvstd %s' % options['tvsys']
+        cmd += ' -target %s-%s' % (options['format'], options['tvsys'])
+    cmd += ' -r %g' % options['fps']
+    cmd += ' -ar %s' % options['samprate']
 
     # Convert scale/expand to ffmpeg's padding system
     if options['scale']:
-        cmd.append('-s %sx%s' % options['scale'])
+        cmd += ' -s %sx%s' % options['scale']
     if options['expand']:
         e_width, e_height = options['expand']
         s_width, s_height = options['scale']
         h_pad = (e_width - s_width) / 2
         v_pad = (e_height - s_height) / 2
         if h_pad > 0:
-            cmd.append('-padleft %s -padright %s' % (h_pad, h_pad))
+            cmd += ' -padleft %s -padright %s' % (h_pad, h_pad)
         if v_pad > 0:
-            cmd.append('-padtop %s -padbottom %s' % (v_pad, v_pad))
+            cmd += ' -padtop %s -padbottom %s' % (v_pad, v_pad)
     if options['widescreen']:
-        cmd.append('-aspect 16:9')
+        cmd += ' -aspect 16:9'
     else:
-        cmd.append('-aspect 4:3')
+        cmd += ' -aspect 4:3'
 
-    cmd.append('-o "%s"' % options['out'])
+    cmd += ' -o "%s"' % options['out']
 
-    cmd.purpose = "Encoding " + infile.filename + " to " + options['format'] + \
-        " " + options['tvsys'] + " format"
-
-    cmd.run()
+    script = Script('ffmpeg_encode')
+    script.append(cmd)
+    return script
--- trunk/tovid/libtovid/encoders/mencoder.py
+++ trunk/tovid/libtovid/encoders/mencoder.py
@@ -5,27 +5,26 @@ __all__ = ['encode']
 
 import logging
 
-from libtovid.cli import Command
+from libtovid.cli import Script
 
 log = logging.getLogger('mencoder.py')
 
-
 def encode(infile, options):
     """Encode infile (a MultimediaFile) with mencoder, using the given options."""
-    cmd = Command('mencoder')
-    cmd.append('"%s" -o "%s"' % (infile.filename, options['out']))
-    cmd.append('-oac lavc -ovc lavc -of mpeg')
+    cmd = 'mencoder'
+    cmd += ' "%s" -o "%s"' % (infile.filename, options['out'])
+    cmd += ' -oac lavc -ovc lavc -of mpeg'
     # Format
     if options['format'] in ['vcd', 'svcd']:
-        cmd.append('-mpegopts format=x%s' % options['format'])
+        cmd += ' -mpegopts format=x%s' % options['format']
     else:
-        cmd.append('-mpegopts format=dvd')
+        cmd += ' -mpegopts format=dvd'
     
     # Audio settings
     # Adjust sampling rate
     # TODO: Don't resample unless needed
-    cmd.append('-srate %s' % options['samprate'])
-    cmd.append('-af lavcresample=%s' % options['samprate'])
+    cmd += ' -srate %s' % options['samprate']
+    cmd += ' -af lavcresample=%s' % options['samprate']
 
     # Video codec
     if options['format'] == 'vcd':
@@ -54,13 +53,13 @@ def encode(infile, options):
     else:
         lavcopts += ':aspect=4/3'
     # Put all lavcopts together
-    cmd.append('-lavcopts %s' % lavcopts)
+    cmd += ' -lavcopts %s' % lavcopts
 
     # FPS
     if options['tvsys'] == 'pal':
-        cmd.append('-ofps 25/1')
+        cmd += ' -ofps 25/1'
     elif options['tvsys'] == 'ntsc':
-        cmd.append('-ofps 30000/1001') # ~= 29.97
+        cmd += ' -ofps 30000/1001' # ~= 29.97
 
     # Scale/expand to fit target frame
     if options['scale']:
@@ -68,8 +67,8 @@ def encode(infile, options):
         # Expand is not done unless also scaling
         if options['expand']:
             vfilter += ',expand=%s:%s' % options['expand']
-        cmd.append('-vf ' + vfilter)
+        cmd += ' -vf ' + vfilter
 
-    cmd.purpose = "Encoding " + infile.filename + " to " + \
-       options['format'] + " " + options['tvsys'] + " format"
-    cmd.run()
+    script = Script('mencoder_encode')
+    script.append(cmd)
+    return script
