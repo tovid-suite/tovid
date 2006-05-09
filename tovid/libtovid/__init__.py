28aa51451e11e4482b30c287b3f2e2261578687e
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-05-09 22:55:10 +0000

    tovid-stats re-added & fixed

--- tovid/libtovid/__init__.py
+++ tovid/libtovid/__init__.py
@@ -17,6 +17,7 @@ __all__ = [\
     'globals',
     'menu',
     'opts',
+    'stats',
     'standards',
     'streams',
     'tdl',
---
+++ tovid/libtovid/stats.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python
+# stats.py
+
+"""Classes and functions for dealing with video statistics."""
+
+import csv
+
+# Order of fields in stats.tovid
+fields = [\
+    'tovid_version',
+    'final_name',
+    'length',          # a.k.a. CUR_LENGTH
+    'format',          # RES
+    'tvsys',
+    'final_size',
+    'tgt_bitrate',
+    'avg_bitrate',
+    'peak_bitrate',
+    'gop_minsize',
+    'gop_maxsize',
+    'encoding_time',   # SCRIPT_TOT_TIME
+    'cpu_model',
+    'cpu_speed']
+
+class Statlist:
+    """A list of VidStats that may be queried with a simple database-like
+    interface."""
+    def __init__(self):
+        stats = []
+
+    def read_csv(self, filename):
+        """Import stats from a CSV (comma-delimited quoted text) file."""
+        self.records = []
+        statfile = open(filename, 'r')
+        csv_reader = csv.DictReader(statfile, fields, skipinitialspace=True)
+        skipped = 0
+        for line in csv_reader:
+            # Format values and append
+            try:
+                line['format'] = str.lower(line['format'] or '')
+                line['tvsys'] = str.lower(line['tvsys'] or '')
+                line['length'] = int(line['length'] or 0)
+                line['avg_bitrate'] = int(line['avg_bitrate'] or 0)
+                line['encoding_time'] = int(line['encoding_time'] or 0)
+            except ValueError:
+                skipped += 1
+            else:
+                self.records.append(line)
+
+        statfile.close()
+        print "Read %s lines from %s" % (len(self.records), filename)
+        print "Skipped %s lines because they contained empty fields." % skipped
+
+    def average(self, attribute):
+        """Calculate the average value for a given numeric VidStat attribute.
+        For example, average('bitrate') returns the average overall bitrate of
+        all videos in the list."""
+        values = []
+        for record in self.records:
+            values.append(record[attribute])
+        if len(values) > 0:
+            return float(sum(values) / len(values))
+        else:
+            return 0
+
+    def average_by(self, attribute, by_attribute):
+        """Return a dictionary of averages of an attribute, indexed by another
+        attribute. For example, average_by('bitrate', 'format') returns average
+        bitrates for each format."""
+        # Create a dictionary of value-lists, indexed by by_attribute
+        values = {}
+        for record in self.records:
+            byval = record[by_attribute]
+            if not values.has_key(byval):
+                values[byval] = []
+            values[byval].append(record[attribute])
+        # Calculate averages for each value-list
+        averages = {}
+        if len(values) > 0:
+            for key, samples in values.iteritems():
+                averages[key] = float(sum(samples) / len(samples))
+        return averages
+
