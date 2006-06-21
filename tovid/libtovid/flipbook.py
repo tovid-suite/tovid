4e10febc79c05fb5c3205afae3b52831e3ff84c8
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-06-21 17:04:20 +0000

    Rephrasing, flipbook demo

--- tovid/libtovid/flipbook.py
+++ tovid/libtovid/flipbook.py
@@ -2,10 +2,12 @@
 # flipbook.py
 
 from libtovid.mvg import Drawing
+from libtovid.layer import Background, Text
+from libtovid.effect import Fade, Movement
 
 class Flipbook:
     """A collection of Drawings that together comprise an animation.
-    It has several frames (or "pages", if you like), 
+    It has several frames (or "pages")
     """
     def __init__(self, frames=30, (width, height)=(720, 576)):
         self.frames = frames
@@ -15,18 +17,30 @@ class Flipbook:
         for frame in range(frames):
             self.drawings.append(Drawing(self.size, 'temp.mvg'))
 
-    def add(self, name, layer):
-        """Add a named Layer to the flipbook."""
-        self.layers[name] = layer
+    def add(self, layer):
+        """Add a Layer to the flipbook."""
+        self.layers.append(layer)
 
-    def get_mvg(self, frame=1):
-        """Get an MVG Drawing for the given frame."""
-        mvg = self.drawings[frame]
-        mvg.clear()
-        for overlay in self.overlays:
-            # Save context and draw the overlay
-            mvg.push('graphic-context')
-            mvg.extend(overlay.get_mvg(frame))
-            mvg.pop('graphic-context')
-        mvg.code()
-        mvg.render()
+    def render(self, frame=1):
+        """Render a Drawing for the given frame."""
+        print "Rendering Flipbook frame %s" % frame
+        drawing = self.drawings[frame]
+        drawing.clear()
+        for layer in self.layers:
+            layer.draw_on(drawing, frame)
+        print drawing.code()
+        drawing.render()
+
+# Demo
+if __name__ == '__main__':
+    print "Flipbook demo"
+    flip = Flipbook()
+    bgd = Background(flip.size, 'red')
+    text = Text("The quick brown fox", (0, 0))
+    text.effects.append(Fade(1, 30, 10))
+    text.effects.append(Movement(1, 30, (100, 100), (500, 300)))
+    flip.add(bgd)
+    flip.add(text)
+    flip.render(1)
+    flip.render(5)
+    flip.render(15)
