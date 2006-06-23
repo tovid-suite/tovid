bfb630c408a05faf569c8066c27d4ab2ef7cacc8
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-06-23 20:14:20 +0000

    Documentation, effect template

--- trunk/tovid/libtovid/effect.py
+++ trunk/tovid/libtovid/effect.py
@@ -1,26 +1,36 @@
 #! /usr/bin/env python
 # effect.py
 
-__all__ = ['Effect', 'Fade', 'Movement', 'Scale', 'Colorfade']
+"""This module defines classes for creating and drawing effects on a series
+of drawings (as in a Flipbook).
+
+Effect classes are arranged in a (currently) simple hierarchy:
+
+    Effect (base class)
+    |-- Movement
+    |-- Fade
+    |-- Colorfade
+    |-- Spectrum
+    |-- Scale
+
+"""
+
+__all__ = ['Effect', 'Movement', 'Fade', 'Colorfade', 'Spectrum', 'Scale']
 
 from libtovid.mvg import Drawing
 from libtovid.animation import Keyframe, tween
 
 class Effect:
-    """A "special effect" created by keyframing MVG draw commands.
-    """
+    """A "special effect" created by keyframing an MVG command along
+    the given frame interval."""
     def __init__(self, start, end):
         """Create an effect lasting from start frame to end frame."""
         self.start = start
         self.end = end
         # List of Keyframes
-        self.keylist = []
+        self.keyframes = [Keyframe(0, 0)]
         # List of tweened values
-        self.data = []
-
-    def tween(self):
-        """Calculate values for all frames from the current keylist."""
-        self.data = tween(self.keylist)
+        self.data = [0]
 
     def get_data(self, frame):
         """Get data value at the given frame."""
@@ -36,6 +46,65 @@ class Effect:
         Override this function in derived classes."""
         pass
 
+# ============================================================================
+# New effect template
+# Copy and paste this code to create your own Effect
+# ============================================================================
+# The first line defines your effect's name. (Effect) means it inherits from
+# the base Effect class.
+class MyEffect (Effect):
+    
+    # The __init__ function is called whenever a MyEffect is created.
+    # Make sure your __init__ takes start and end arguments; additional
+    # arguments (such as start_val and end_val below) allow someone using
+    # your effect class to customize its behavior in some way. See the
+    # other effects below for examples.
+    def __init__(self, start, end, start_val, end_val):
+        """Create a MyEffect lasting from start to end frame.
+        Modify this documentation string to describe what your effect does.
+        """
+        # Be sure to call this first-thing:
+        Effect.__init__(self, start, end)
+        # It initializes the base Effect class with start and end frames.
+
+        # Next, define any keyframes your effect needs to use. This
+        # effect just varies something from start_val to end_val:
+        self.keyframes = [\
+            Keyframe(start, start_val),
+            Keyframe(end, end_val)
+            ]
+
+        # Call this afterwards, to calculate the values at all frames
+        self.data = tween(self.keyframes)
+
+    # Now, override the draw_on function...
+    def draw_on(self, drawing, frame):
+        # ...and use a MVG Drawing command, passing the data
+        # for the current frame. Replace 'translate' with your
+        # own drawing function (see libtovid/mvg.py for a complete list)
+        drawing.translate(self.get_data(frame))
+
+    # That's it! Your effect is ready to use.
+    # See libtovid/flipbook.py for examples on how to use effects
+
+# ============================================================================
+# End of new effect template
+# ============================================================================
+
+class Movement (Effect):
+    """A movement effect, from one point to another."""
+    def __init__(self, start, end, (x0, y0), (x1, y1)):
+        """Move from start (x0, y0) to end (x1, y1)."""
+        Effect.__init__(self, start, end)
+        self.keyframes = [\
+            Keyframe(start, (x0, y0)),
+            Keyframe(end, (x1, y1))
+            ]
+        self.data = tween(self.keyframes)
+
+    def draw_on(self, drawing, frame):
+        """Draw the movement effect into the given Drawing."""
+        drawing.translate(self.get_data(frame))
 
 class Fade (Effect):
     """A fade-in/fade-out effect, varying the opacity of a layer."""
@@ -50,59 +119,28 @@ class Fade (Effect):
         #
         # The tween()ed array is indexed as an offset from the start
         # frame (self.opacities[0] gives the value at the start frame)
-        self.keylist = [\
+        self.keyframes = [\
             Keyframe(start, 0.0),                  # Start fading in
             Keyframe(start + fade_length, 1.0),    # Fade-in done
             Keyframe(end - fade_length, 1.0),      # Start fading out
             Keyframe(end, 0.0)                     # Fade-out done
             ]
-        self.tween()
+        self.data = tween(self.keyframes)
 
     def draw_on(self, drawing, frame):
         """Draw the fade effect into the given Drawing."""
         drawing.fill_opacity(self.get_data(frame))
-
-
-class Movement (Effect):
-    """A movement effect, from one point to another."""
-    def __init__(self, start, end, (x0, y0), (x1, y1)):
-        """Move from start (x0, y0) to end (x1, y1)."""
-        Effect.__init__(self, start, end)
-        self.keylist = [\
-            Keyframe(start, (x0, y0)),
-            Keyframe(end, (x1, y1))
-            ]
-        self.tween()
-
-    def draw_on(self, drawing, frame):
-        """Draw the movement effect into the given Drawing."""
-        drawing.translate(self.get_data(frame))
-
-
-class Scale (Effect):
-    """A Scaling effect, from one size to another."""
-    def __init__(self, start, end, (w0, h0), (w1, h1)):
-        Effect.__init__(self, start, end)
-        self.keylist =[\
-            Keyframe(start, (w0, h0)),
-            Keyframe(end, (w1, h1))
-            ]
-        self.tween()
-
-    def draw_on(self, drawing, frame):
-        drawing.scale(self.get_data(frame))
-
 
 class Colorfade (Effect):
     """A color-slide effect between an arbitrary number of RGB colors."""
     def __init__(self, start, end, (r0, g0, b0), (r1, g1, b1)):
         """Fade between the given RGB colors."""
         Effect.__init__(self, start, end)
-        self.keylist = [\
+        self.keyframes = [\
             Keyframe(start, (r0, g0, b0)),
             Keyframe(end, (r1, g1, b1))
             ]
-        self.tween()
+        self.data = tween(self.keyframes)
 
     def draw_on(self, drawing, frame):
         drawing.fill_rgb(self.get_data(frame))
@@ -112,7 +150,7 @@ class Spectrum (Effect):
     def __init__(self, start, end):
         Effect.__init__(self, start, end)
         step = (end - start) / 6
-        self.keylist = [\
+        self.keyframes = [\
             Keyframe(start, (255, 0, 0)),
             Keyframe(start + step, (255, 0, 255)),
             Keyframe(start + step*2, (0, 0, 255)),
@@ -121,24 +159,25 @@ class Spectrum (Effect):
             Keyframe(start + step*5, (255, 255, 0)),
             Keyframe(end, (255, 0, 0))
             ]
-        self.tween()
+        self.data = tween(self.keyframes)
 
     def draw_on(self, drawing, frame):
         drawing.fill_rgb(self.data[frame - self.start])
 
 
-# Scaling effects
-class Appear (Scale):
-    """An "appear from nowhere" effect, quickly scaling up from a point."""
-    def __init__(self, frame):
-        """Appear starting at the given frame, with a 10-frame scale-in."""
-        Scale.__init__(self, frame, frame + 10, (0.0, 0.0), (1.0, 1.0))
+class Scale (Effect):
+    """A Scaling effect, from one size to another."""
+    def __init__(self, start, end, (w0, h0), (w1, h1)):
+        Effect.__init__(self, start, end)
+        self.keyframes =[\
+            Keyframe(start, (w0, h0)),
+            Keyframe(end, (w1, h1))
+            ]
+        self.data = tween(self.keyframes)
 
-class Disappear (Scale):
-    """A disappearance effect, scaling down to a point. Opposite of Appear."""
-    def __init__(self, frame):
-        """Disappear finishing at the given frame, with a 10-frame scale-out."""
-        Scale.__init__(self, frame - 10, frame, (1.0, 1.0), (0.0, 0.0))
+    def draw_on(self, drawing, frame):
+        drawing.scale(self.get_data(frame))
+
 
 
 # Demo
