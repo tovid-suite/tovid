d4e3c3a585b017cce0cac5863d5a3fead6cbbcd2
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-06-29 21:45:35 +0000

    Random rearrangement, get_type function

--- trunk/tovid/libtovid/utils.py
+++ trunk/tovid/libtovid/utils.py
@@ -1,43 +1,39 @@
 #! /usr/bin/env python
 # utils.py
 
-# TODO: Categorize/reorganize these
+# TODO: Categorize/reorganize these (or move some to a class)
 
-__all__ = ['escape', 'tokenize', 'trim', 'ratio_to_float',
-    'pretty_dict', 'indent_level', 'get_code_lines', 'wait']
+__all__ = [\
+    'escape',
+    'float_to_ratio',
+    'get_code_lines',
+    'get_type',
+    'indent_level',
+    'pretty_dict',
+    'ratio_to_float',
+    'tokenize',
+    'trim',
+    'wait'
+    ]
 
 # From standard library
 import os
 import sys
 import shlex
+import mimetypes
 
 def escape(text):
     """Return a copy of the given text string with potentially problematic
     "special" characters backslash-escaped."""
-    special_chars = ' *:;?![]()"\''
+    special_chars = ' *:;?![]()"\'\\'
     result = text
     for char in special_chars:
         result = result.replace(char, '\%s' % char)
     return result
 
-def tokenize(line, include_chars=''):
-    """Separate a text line into tokens, returning them in a list. By default,
-    tokens are space-separated, and each token consists of [a-z], [A-Z], [0-9],
-    or any of '.:-%()/'. Additional valid token characters may be specified by
-    passing them in the include_chars string."""
-    lexer = shlex.shlex(line, posix = True)
-    # Rules for splitting tokens
-    lexer.wordchars = lexer.wordchars + '.:-%()/' + include_chars
-    lexer.whitespace_split = False
-    # Append all tokens to a list
-    tokens = []
-    while True:
-        token = lexer.get_token()
-        if not token:
-            break
-        else:
-            tokens.append(token)
-    return tokens
+def indent_level(line):
+    """Return the number of leading whitespace characters in the line."""
+    return len(line) - len(line.lstrip())
 
 def trim(text):
     """Strip leading indentation from a block of text.
@@ -67,7 +63,6 @@ def trim(text):
         trimmed.pop()
     # Return a string, rejoined with newlines
     return '\n'.join(trimmed)
-    
 
 def ratio_to_float(ratio):
     """Convert a string expressing a numeric ratio, with X and Y parts
@@ -93,6 +88,24 @@ def float_to_ratio(number):
     numerator = float(number) * 1000
     return "%g:1000" % numerator
 
+def tokenize(line, include_chars=''):
+    """Separate a text line into tokens, returning them in a list. By default,
+    tokens are space-separated, and each token consists of [a-z], [A-Z], [0-9],
+    or any of '.:-%()/'. Additional valid token characters may be specified by
+    passing them in the include_chars string."""
+    lexer = shlex.shlex(line, posix = True)
+    # Rules for splitting tokens
+    lexer.wordchars = lexer.wordchars + '.:-%()/' + include_chars
+    lexer.whitespace_split = False
+    # Append all tokens to a list
+    tokens = []
+    while True:
+        token = lexer.get_token()
+        if not token:
+            break
+        else:
+            tokens.append(token)
+    return tokens
 
 def pretty_dict(dict):
     """Return a pretty-printed dictionary, with one line for each key."""
@@ -112,10 +125,6 @@ def pretty_dict(dict):
             result += "    %s %s\n" % (key, value)
     return result
 
-def indent_level(line):
-    """Return the number of leading whitespace characters in the line."""
-    return len(line) - len(line.lstrip())
-
 def get_code_lines(filename):
     """Return a list of all lines of code in the given file.
     Whitespace and #-style comments are ignored."""
@@ -131,3 +140,18 @@ def wait(seconds):
     """Print a message and pause for the given number of seconds."""
     print "Resuming in %s seconds..." % seconds
     os.system('sleep %ss' % seconds)
+
+def get_type(filename):
+    """Return 'image', 'audio', or 'video', if the given filename appears to be
+    any of those types; otherwise, return None. Determined by file's mimetype,
+    which is based on filename extension, so possibly inaccurate. Will return
+    None for any directory or extensionless filename.
+    """
+    type, encoding = mimetypes.guess_type(filename)
+    # Get the base type (the part before '/')
+    if type:
+        basetype = type[0:type.find('/')]
+    if basetype in ['image', 'audio', 'video']:
+        return basetype
+    else:
+        return None
