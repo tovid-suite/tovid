3a5c8bfb2641423268196cec647b55df7cf72d1e
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-07-18 02:38:15 +0000

    More reworking, removals

--- trunk/tovid/libtovid/standards.py
+++ trunk/tovid/libtovid/standards.py
@@ -2,54 +2,23 @@
 # standards.py
 
 """This module defines functions for retrieving information about multimedia
-standards. Any data about widely-published standards should be defined here,
-for use by all libtovid modules.
+standards, including functions for determining the appropriate resolution,
+video and audio codec, fps, and bitrates for a given format.
 
-   >>> standards.is_compliant('foo.mpg' 'dvd', 'ntsc')
-   True
-   >>> standards.compliance('foo.mpg')
-   ['dvd', 'ntsc']
-   
+Known formats include VCD, SVCD, DVD, Half-DVD, and VCD-on-DVD.
 """
 
 __all__ = [\
     'get_resolution',
-    'get_codec',
+    'get_vcodec',
+    'get_acodec',
     'get_fps',
     'VideoStandard',
     'AudioStandard']
 
-# TODO: This module needs major cleanup, ruthless code-culling and
-# overall simplification.
-
 import doctest
 from libtovid.media import VideoStream, AudioStream
 
-# Dictionaries of standard attributes for easy lookup
-format_size = {\
-    'vcd':
-        {'pal': (352, 288), 'ntsc': (352, 240)},
-    'dvd-vcd':
-        {'pal': (352, 288), 'ntsc': (352, 240)},
-    'svcd':
-        {'pal': (480, 576), 'ntsc': (480, 480)},
-    'half-dvd':
-        {'pal': (352, 576), 'ntsc': (352, 480)},
-    'dvd':
-        {'pal': (720, 576), 'ntsc': (720, 480)}
-    }
-format_bitrate = {\
-    'vcd': 1152,
-    'svcd': (0, 2600),
-    'dvd-vcd': (0, 9800),
-    'half-dvd': (0, 9800),
-    'dvd': (0, 9800)
-    }
-tvstd_fps = {\
-    'pal': 25.00,
-    'ntsc': 29.97,
-    'ntscfilm': 23.976
-    }
 
 def get_resolution(format, tvsys):
     """Return the pixel resolution (x,y) for the given format and TV system.
@@ -60,101 +29,132 @@ def get_resolution(format, tvsys):
         >>> get_resolution('half-dvd', 'ntsc')
         (352, 480)
     """
-    return valid_resolutions[format][tvsys]
+    # Valid resolutions, indexed by format and tvsys
+    valid_size = {\
+        'vcd':
+            {'pal': (352, 288), 'ntsc': (352, 240)},
+        'dvd-vcd':
+            {'pal': (352, 288), 'ntsc': (352, 240)},
+        'svcd':
+            {'pal': (480, 576), 'ntsc': (480, 480)},
+        'half-dvd':
+            {'pal': (352, 576), 'ntsc': (352, 480)},
+        'dvd':
+            {'pal': (720, 576), 'ntsc': (720, 480)}
+        }
+    return valid_size[format][tvsys]
 
 
-def get_codec(format, tvsys):
-    """Return the codec used by the given format and TV system. For example:
+def get_vcodec(format):
+    """Return the video codec used by the given format. For example:
     
-        >>> get_codec('vcd', 'ntsc')
+        >>> get_vcodec('vcd')
         mpeg1
-        >>> get_codec('svcd', 'ntsc')
+        >>> get_vcodec('svcd')
         mpeg2
     """
     if format == 'vcd':
         return 'mpeg1'
     else:
         return 'mpeg2'
+
+
+def get_acodec(format):
+    """Return the audio codec (or codecs) supported by the given format. For
+    example:
+    
+        >>> get_acodec('vcd')
+        mp2
+        >>> get_acodec('dvd')
+        ['ac3', 'mp2', 'pcm']
+    """
+    if format in ['vcd', 'svcd']:
+        return 'mp2'
+    else:
+        return ['ac3', 'mp2', 'pcm']
+    
+def get_samprate(format):
+    """Return the audio sampling rate used by the given format."""
+    if format in ['vcd', 'svcd']:
+        return 44100
+    else:
+        return 48000
     
 
 def get_fps(format, tvsys):
-    """Return the number of frames per second for the given format and TV
-    system. For example:
+    """Return the number of frames per second for the given TV system. For
+    example:
     
-        >>> get_fps('dvd', 'ntsc')
+        >>> get_fps('ntsc')
         29.97
-        >>> get_fps('dvd', 'pal')
+        >>> get_fps('pal')
         25.00
     """
-    return tvstd_fps[tvsys]
+    # Valid frames per second, by TV system
+    fps = {\
+        'pal': 25.00,
+        'ntsc': 29.97,
+        'ntscfilm': 23.976
+        }
+    return fps[tvsys]
 
 
-def get_bitrate_range(format):
-    """Return the range (min, max) of valid bitrates for the given format.
-    For example:
+def get_vbitrate(format):
+    """Return the range (min, max) of valid video bitrates (in kilobits per
+    second) for the given format, or a single value for constant-bitrate
+    formats. For example:
 
-        >>> get_bitrate_range('dvd')
+        >>> get_vbitrate('dvd')
         (0, 9800)
-        >>> get_bitrate_range('vcd')
-        (1152, 1152)
+        >>> get_vbitrate('vcd')
+        1152
     """
-    return format_bitrate[format]
+    # Valid video bitrates, indexed by format
+    valid_bitrates = {\
+        'vcd': 1152,
+        'svcd': (0, 2600),
+        'dvd-vcd': (0, 9800),
+        'half-dvd': (0, 9800),
+        'dvd': (0, 9800)
+        }
+    return valid_bitrates[format]
+
+def get_abitrate(format):
+    """Return the range (min, max) of valid audio bitrates (in kilobits per
+    second) for the given format, or a single value for constant-bitrate
+    formats. For example:
+    
+        >>> get_abitrate('vcd')
+        224
+        >>> get_abitrate('dvd')
+        (32, 1536)
+    """
+    if format == 'vcd':
+        return 224
+    elif format == 'svcd':
+        return (32, 384)
+    else:
+        return (320, 1536)
+        
 
 
 def is_compliant(filename, format, tvsys):
     """Return True if the given file is compliant with the given format and
     TV system, False otherwise.
     """
+    # TODO
     pass
+
 
 def compliance(filename):
     """Return standards-compliance information about the given file, as a list
     of keywords."""
+    # TODO
     pass
 
 
-def validate_specs(video, audio):
-    """Return a list of audio and video standards matched by the given specs."""
-    assert isinstance(video, VideoStream)
-    assert isinstance(audio, AudioStream)
-    # Find any standards matching video's width and height
-    width = video.width
-    height = video.height
-    if width in std_widths:
-        if height in std_heights:
-            # Find all formats matching both width and height
-            formats = filter(lambda x:x not in std_widths[width],
-                             std_heights[height])
-            print "Matching %sx%s resolution:" % (width, height)
-            print formats
 
-
-def video_is_valid(vstream, vstandard):
-    """Return True if the video stream is valid under the given standard."""
-    if (vstream.width == vstandard.width and
-         vstream.height == vstandard.height and
-         vstream.fps == vstandard.fps and
-         vstream.codec == vstandard.codec and
-         vstream.bitrate >= vstandard.minBitrate and
-         vstream.bitrate <= vstandard.maxBitrate):
-        return True
-    else:
-        return False
-
-def audio_is_valid(astream, astandard):
-    """Return True if the audio stream is valid under the given standard."""
-    if (astream.bitrate >= astandard.minBitrate and
-         astream.bitrate <= astandard.maxBitrate and
-         astream.codec == astandard.codec and
-         astream.samprate == astandard.samprate):
-        return True
-    else:
-        return False
-
-# ===========================================================
-# TODO: Merge stuff above with stuff below somehow...
-# ===========================================================
-
+# Currently unused stuff...
 class VideoStandard:
     def __init__(self, keywords = [], codec = "",
                   resolution = (0, 0), fps = 0, bitrateRange = (0, 0)):
@@ -235,7 +235,7 @@ AudioStandardList = [
     AudioStandard(["vcd", "ntsc"], "mp2", 44100, 2, (224, 224)),
 
     # SVCD standard formats
-    AudioStandard(["svcd", "pal"], "mp2", 44100, 2, (32, 1536)),
+    AudioStandard(["svcd", "pal"], "mp2", 44100, 2, (32, 384)),
     AudioStandard(["svcd", "ntsc"], "mp2", 44100, 2, (32, 384)),
 
     # DVD standard formats
