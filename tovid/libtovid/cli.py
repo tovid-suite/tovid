f59b0221e5bf09e0453a849a811cb34bae560b05
cogumbreiro <cogumbreiro@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-08-22 15:29:56 +0000

    Added a new class for generating commands safely. Made option variables that appear on scripts appear commented, since they are not used.

--- tovid/libtovid/cli.py
+++ tovid/libtovid/cli.py
@@ -15,7 +15,6 @@ __all__ = ['Command', 'Script', 'verify_app']
 import os
 import sys
 import tempfile
-import commands
 from stat import S_IREAD, S_IWRITE, S_IEXEC
 from signal import SIGKILL
 # From libtovid
@@ -124,7 +123,7 @@ class Script:
         text += 'cat %s\n' % self.script_file
         # TODO: 
         for var, value in self.locals.iteritems():
-            text += '%s=%s\n' % (var, value)
+            text += '# %s=%s\n' % (var, value)
         for cmd in self.commands:
             text += '%s\n' % cmd
         text += 'exit\n'
@@ -167,4 +166,124 @@ def verify_app(appname):
         sys.exit()
         
      
+def enc_arg(arg):
+    """Converts it to a string and encodes an argument making it safe 
+    from special bash characters"""
+    arg = str(arg)
+    for special_char in '# "\'\\&|<>()':
+        if special_char in arg:
+            return '"%s"' % arg.replace('\\', '\\\\').replace('"', '\\"')
+    return arg
+
+class Bg(object):
+    """
+    This makes sure there is only one 'Arg' in backgrond.
+    """
+    def __init__(self, arg):
+        if not isinstance(arg, Arg):
+            raise TypeError("must be an Arg")
+            
+        self.arg = arg
     
+    def __str__(self):
+        return str(self.arg) + " &"
+    
+    def __repr__(self):
+        return "Bg(%r)" % self.arg
+
+class Pipe(object):
+    """Represents a pipe object, makes sure no extra operations
+    are performed to a piped command."""
+    
+    def __init__(self, first, after):
+        self.first = first
+        self.after = after
+    
+    def __repr__(self):
+        return "Pipe(%r, %r)" % (self.first, self.after)
+    
+    def __str__(self):
+        return "%s | %s" % (self.first, self.after)
+
+class Arg(object):
+    """An object used for creating commands used on shell scripts.
+    It encodes the arguments automagically. Examples::
+
+        >>> Arg('echo')
+        Arg('echo')
+        >>> echo = Arg('echo')
+        >>> echo.add('foo', 'bar')
+        >>> echo
+        Arg('echo foo bar')
+        >>> Arg("echo") + 'foo and bar'
+        Arg('echo "foo and bar"')
+        >>> echo = Arg("echo").add('foo and bar')
+        >>> echo
+        Arg('echo "foo and bar"')
+        >>> echo += "baz"
+        Arg('echo "foo and bar" baz')
+        >>> str(echo)
+        'echo "foo and bar" baz'
+        
+    """
+    def __init__(self, arg="", stdin=None, stdout=None, stderr=None):
+        self.arg = arg
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+
+    def __add__(self, other):
+        return Arg(self.arg + " %s" % enc_arg(other))
+    
+    def add(self, *others):
+        """Adds a number of strings, each argument is a command argument
+        and will be encoded as such. This method returns the 'self'"""
+        self.arg += " %s" % " ".join(map(enc_arg, others))
+        return self
+
+    def addraw(self, raw):
+        """the 'raw' argument must be a string and will be stripped of
+        extra whitechars. Returns itself."""
+        self.arg += " %s" % raw.strip()
+        return self
+
+    def pipe(self, other):
+        """Creates a new Arg object which results on the pipe between this
+        and the other program."""
+        if self.stdout is not None:
+            raise TypeError("Cannot pipe if output was redirected to a file.")
+        if other.stdin is not None:
+            raise TypeError("Cannot pipe if input of other process is redirected to a file.")
+        
+        # TODO: make sure the other's stdout is not mixed with the generated
+        # stdout
+        return Arg("%s | %s" % (str(self), str(other)))
+
+    def to_bg(self):
+        """Makes this command run in background, returns itself."""
+        return Bg(self)
+
+    def read_from(self, filename):
+        self.stdin = filename
+
+    def write_to(self, filename):
+        self.stdout = filename
+
+    def errors_to(self, filename):
+        self.stderr = filename
+
+    def __str__(self):
+        ret = self.arg
+        if self.stdin is not None:
+            ret += " < %s" % enc_arg(self.stdin)
+        if self.stdout is not None:
+            ret += " > %s" % enc_arg(self.stdout)
+        if self.stderr is not None:
+            ret += " 2> %s" % enc_arg(self.stderr)
+            
+        return ret
+
+    def __repr__(self):
+        return "Arg(%r, %r, %r, %r)" % (self.arg, self.stdin, self.stdout,
+                                        self.stderr)
+      
