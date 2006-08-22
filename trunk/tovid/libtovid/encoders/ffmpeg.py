a86c21efdd0b7dd9f978fbbaadb73b1427e2489c
cogumbreiro <cogumbreiro@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-08-22 15:31:26 +0000

    Made the encoders use the `libtovid.cli.Arg` class instead of raw strings.

--- trunk/tovid/libtovid/encoders/ffmpeg.py
+++ trunk/tovid/libtovid/encoders/ffmpeg.py
@@ -3,7 +3,7 @@
 
 __all__ = ['get_script']
 
-from libtovid.cli import Script
+from libtovid.cli import Script, Arg
 from libtovid.log import Log
 
 log = Log('libtovid.encoders.ffmpeg')
@@ -15,31 +15,35 @@ def get_script(infile, options):
     script = Script('ffmpeg')
 
     # Build the ffmpeg command as a string
-    cmd = 'ffmpeg'
-    cmd += ' -i "%s"' % infile.filename
+    cmd = Arg('ffmpeg')
+    cmd.add('-i', infile.filename)
     if options['format'] in ['vcd', 'svcd', 'dvd']:
-        cmd += ' -tvstd %s' % options['tvsys']
-        cmd += ' -target %s-%s' % \
-                (options['tvsys'], options['format'])
-    cmd += ' -r %s' % options['fps']
-    cmd += ' -ar %s' % options['samprate']
+        cmd.add('-tvstd', options['tvsys'])
+        cmd.add('-target', '%s-%s' % \
+                (options['tvsys'], options['format']))
+    
+    cmd.add('-r', options['fps'])
+    cmd.add('-ar', options['samprate'])
     # Convert scale/expand to ffmpeg's padding system
     if options['scale']:
-        cmd += ' -s %sx%s' % options['scale']
+        cmd.add('-s', '%sx%s' % options['scale'])
     if options['expand']:
         e_width, e_height = options['expand']
         s_width, s_height = options['scale']
         h_pad = (e_width - s_width) / 2
         v_pad = (e_height - s_height) / 2
         if h_pad > 0:
-            cmd += ' -padleft %s -padright %s' % (h_pad, h_pad)
+            cmd.add('-padleft', h_pad)
+            cmd.add('-padright', h_pad)
         if v_pad > 0:
-            cmd += ' -padtop %s -padbottom %s' % (v_pad, v_pad)
+            cmd.add('-padtop', v_pad)
+            cmd.add('-padbottom', v_pad)
     if options['widescreen']:
-        cmd += ' -aspect 16:9'
+        cmd.add('-aspect', '16:9')
     else:
-        cmd += ' -aspect 4:3'
-    cmd += ' "%s"' % options['out']
+        cmd.add('-aspect', '4:3')
+    
+    cmd.add(options['out'])
 
-    script.append(cmd)
+    script.append(str(cmd))
     return script
