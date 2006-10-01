bd81908c124cdc9041e753c85b5c22f3e627a8a8
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-10-01 17:10:58 +0000

    Documentation, doctest

--- trunk/tovid/libtovid/log.py
+++ trunk/tovid/libtovid/log.py
@@ -3,12 +3,46 @@
 
 """This module defines a Log class, providing a wrapper around the standard
 Python 'logging' module.
+
+Using this module is similar to using Python's built-in logging module, with
+the exception that some of the initial setup procedures are handled
+automatically. Simply create a Log instance with a name and logging level:
+
+    >>> log = Log('MyApp', 'info')
+
+The logging level determines output verbosity. Use "info" for normal program
+output, "debug" for debugging messages, "error" for important error messages,
+and so on:
+
+    >>> log.info("Starting app...")
+    Starting app...
+    >>> log.debug("Pointer is 0x1582")
+    >>> log.error("An error occurred")
+    An error occurred
+
+To change the verbosity level, use setLevel:
+
+    >>> log.setLevel('debug')
+    >>> log.debug("Pointer is 0x1582")
+    Pointer is 0x1582
+
+To disable all output except for "critical" messages:
+
+    >>> log.setLevel('critical')
+    >>> log.debug("Pointer is 0x1582")
+    >>> log.info("Starting app...")
+    >>> log.warning("Deprecation warning")
+    >>> log.error("An error occurred")
+    >>> log.critical("This is really, really bad!")
+    This is really, really bad!
+
 """
 
 import sys
 import logging
+import doctest
 
-# Logging levels
+# Logging levels, in increasing order of severity
 levels = {
     'debug': logging.DEBUG,
     'info': logging.INFO,
@@ -27,6 +61,7 @@ class Log:
         self.setLevel(level)
         # Standard output logger
         self.logger.addHandler(logging.StreamHandler(sys.stdout))
+        # TODO: Add an interface for file logging
 
     def setLevel(self, level):
         """Set log verbosity level to one of 'debug', 'info', 'warning',
@@ -36,21 +71,24 @@ class Log:
         self.logger.setLevel(levels[level])
 
     def debug(self, message):
-        """Log a debugging message."""
+        """Log a debugging message (low priority)."""
         self.logger.debug(message)
         
     def info(self, message):
-        """Log an informational message."""
+        """Log an informational message (normal priority)."""
         self.logger.info(message)
 
-    def warn(self, message):
-        """Log a warning message."""
+    def warning(self, message):
+        """Log a warning message (medium priority)."""
         self.logger.warning(message)
 
     def error(self, message):
-        """Log an error message."""
+        """Log an error message (high priority)."""
         self.logger.error(message)
 
     def critical(self, message):
-        """Log a critical message."""
+        """Log a critical message (highest priority)."""
         self.logger.critical(message)
+
+if __name__ == '__main__':
+    doctest.testmod(verbose=True)
