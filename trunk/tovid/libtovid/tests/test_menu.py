d120d2db0789008e9ca5250da9e40abad3dea881
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2006-11-18 06:47:37 +0000

    Renamed standards.py to standard.py, renamed get_foo() to foo() to simplify frontend

--- trunk/tovid/libtovid/tests/test_menu.py
+++ trunk/tovid/libtovid/tests/test_menu.py
@@ -7,17 +7,17 @@ import sys
 sys.path.insert(0, '..')
 # Get modules to test
 from libtovid.layout import menu
-from libtovid import standards
+from libtovid import standard
 
 
 class TestMenu(unittest.TestCase):
     def test_preproc(self):
-        """Test if the preproc grabs it's arguments from standards"""
+        """Test if the preproc grabs its arguments from standard"""
         m = menu.Menu({'format': 'dvd', 'tvsys': 'ntsc'})
         m.preproc()
 
-        w, h = standards.get_resolution('dvd', 'ntsc')
-        r = standards.get_samprate('dvd')
+        w, h = standard.resolution('dvd', 'ntsc')
+        r = standard.samprate('dvd')
         
         self.assertEquals(m.options['samprate'], r)
         self.assertEquals(m.options['expand'], (w, h))
