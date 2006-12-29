fb7005b8ca0a7fe944154382c6051ffe0729727b
wapcaplet88 <wapcaplet88@gmail.com>
2006-12-29 00:55:50 +0000

    Indentation, more docstrings and examples

--- tovid/libtovid/xml.py
+++ tovid/libtovid/xml.py
@@ -3,32 +3,56 @@
 
 """This module is for defining XML elements and their valid attributes.
 
-To define an element 'video', which may have attributes called 'file' and
-'length', do:
+To define an element class Video, which may have attributes called 'file'
+and 'length', do:
 
     >>> Video = create_element('video', ['file', 'length'])
 
-Video is now an Element class, which can be used to instantiate new video
+Video is now an Element subclass, which can be used to instantiate new video
 elements:
 
-    >>> video1 = Video(file='Brian.mpg', length=4)
+    >>> video1 = Video(file='Brian.mpg')
     >>> video2 = Video(file='Judith.mpg')
 
 Attributes may be changed via the set method:
 
-    >>> video2.set(length=5)
+    >>> video1.set(length=10)
 
 To get an XML representation of an Element:
 
     >>> print video1
-    <video file="Brian.mpg" length="4"/>
+    <video file="Brian.mpg" length="10"/>
     >>> print video2
-    <video file="Judith.mpg" length="5"/>
-    
+    <video file="Judith.mpg"/>
+
+To create an Element class with no attributes, use the empty list:
+
+    >>> Group = create_element('group', [])
+
+Elements can contain other elements, to create a hierarchy:
+
+    >>> group = Group()
+    >>> group.add_child(video1)
+    >>> group.add_child(video2)
+    >>> print group
+    <group>
+      <video file="Brian.mpg" length="10"/>
+      <video file="Judith.mpg"/>
+    </group>
+
+An Element may also contain plain-text content:
+
+    >>> video1.content = "Always look on the bright side of life"
+    >>> print video1
+    <video file="Brian.mpg" length="10">Always look on the bright side of life
+    </video>
+
+See spumux.py for additional examples.
 """
 
 __all__ = [\
     'Element',
+    'format',
     'create_element']
 
 class Element (object):
@@ -48,13 +72,13 @@ class Element (object):
         
             attributes: Dictionary of attributes matching those in ATTRIBUTES
             kwargs:     Keyword arguments for setting specific attributes
-                        NOTE: Hyphenated attribute names can't be used here
+                        (NOTE: Hyphenated attribute names can't be used here)
         """
         self.attributes = {}
         self.children = []
+        self.content = ''
         # Set attributes from those provided
         self.set(attributes, **kwargs)
-
 
     def set(self, attributes=None, **kwargs):
         """Set values for one or more attributes.
@@ -77,34 +101,60 @@ class Element (object):
                 raise KeyError("'%s' element has no attribute '%s'" %
                                (self.NAME, key))
 
-
     def add_child(self, element):
         """Add the given Element as a child of this Element."""
         assert isinstance(element, Element)
         self.children.append(element)
 
-
-    def __str__(self):
-        """Return a string containing XML for the Element."""
-        # TODO: Pretty indentation
+    def xml(self):
+        """Return a string containing raw XML for the Element."""
         text = '<%s' % self.NAME
         # Iterate through valid attributes so ordering is preserved
         for key in self.ATTRIBUTES:
             if key in self.attributes:
                 text += ' %s="%s"' % (key, self.attributes[key])
-        if self.children:
+        # If children or text content are present, add them with a closing tag
+        if self.children or self.content:
             text += '>'
+            text += str(self.content)
             for child in self.children:
-                text += '\n%s' % child
-            text += '\n</%s>' % self.NAME
+                text += child.xml()
+            text += '</%s>' % self.NAME
+        # No children or content; use "empty" element closing tag
         else:
             text += '/>'
         return text
 
+    def __str__(self):
+        """Return a string containing formatted XML for the Element."""
+        return format(self.xml())
+
+
+def format(xml_text):
+    """Return the given XML text string, formatted with appropriate
+    line-breaks and indentation.
+    """
+    indent = -1
+    result = ''
+    text = xml_text.replace('<', '\n<').lstrip('\n')
+    for line in text.split('\n'):
+        if not line.startswith('</'):
+            indent += 1
+        result += '  ' * indent + line + '\n'
+        if line.startswith('</') or line.endswith('/>'):
+            indent -= 1
+    return result.rstrip('\n')
+
 
 def create_element(name, valid_attributes):
-    """Create a new Element subclass, having the given name and
-    list of valid attributes.
+    """Create a new Element subclass.
+
+        name:              Element name
+        valid_attributes:  List of valid attribute names
+
+    Returns a subclass of Element; assign the return value of this function
+    to a variable to create your own Element subclass, which may then be used
+    to instantiate new Elements.
     """
     return type(name, (Element,),
                 {'NAME': name, 'ATTRIBUTES': valid_attributes})
