52602211ee3a053e61e3b22828df223700d92046
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2007-01-08 02:54:54 +0000

    Remove old stuff

--- tovid/libtovid/author/vcdimager.py
+++ tovid/libtovid/author/vcdimager.py
@@ -2,107 +2,12 @@
 # vcdimager.py
 
 """This module provides functions for authoring a video CD using vcdimager.
-
 """
 
-# __all__ = []
+__all__ = ['get_xml']
 
 from libtovid import xml
 from libtovid import layout
-
-def get_xml(disc):
-    """Return vcdxbuild XML suitable for authoring the given Disc.
-    """
-    xml = """<?xml version="1.0"?>
-    <!DOCTYPE videocd PUBLIC "-//GNU/DTD VideoCD//EN"
-      "http://www.gnu.org/software/vcdimager/videocd.dtd">
-    <videocd xmlns="http://www.gnu.org/software/vcdimager/1.0/"
-    """
-    # format == vcd|svcd, version=1.0 (svcd) | 2.0 (vcd)
-    assert disc.format in ['vcd', 'svcd']
-    if disc.format == 'vcd':
-        xml += 'class="vcd" version="2.0">\n'
-    elif disc.format == 'svcd':
-        xml += 'class="svcd" version="1.0">\n'
-
-    if disc.format == 'svcd':
-        xml += '<option name="update scan offsets" value="true" />'
-
-    xml += """<info>
-      <album-id>VIDEO_DISC</album-id>
-      <volume-count>1</volume-count>
-      <volume-number>1</volume-number>
-      <restriction>0</restriction>
-    </info>
-    <pvd>
-      <volume-id>VIDEO_DISC</volume-id>
-      <system-id>CD-RTOS CD-BRIDGE</system-id>
-    </pvd>
-    """
-    
-    # TODO:
-    # segment-items
-    # sequence-items
-    # pbc + selections
-    xml += '</videocd>'
-
-
-# vcdimager XML element definitions
-"""
-videocd ['xmlns', 'class', 'version']
-option ['name', 'value']
-info
-    album-id
-    volume-count
-    volume-number
-    next-volume-use-sequence2
-    next-volume-use-lid2
-    restriction
-    time-offset
-pvd
-    volume-id
-    system-id
-    application-id
-    preparer-id
-    publisher-id
-
-segment-items
-    segment-item ['src', 'id']
-sequence-items
-    sequence-item ['src', 'id']
-        entry ['id']
-        auto-pause
-pbc
-    playlist ['id']
-        prev ['ref']
-        next ['ref']
-        return ['ref']
-        playtime
-        wait
-        autowait
-        play-item ['ref']
-    selection ['id']
-        bsn
-        prev ['ref']
-        next ['ref']
-        return ['ref']
-        default ['ref']
-        timeout ['ref']
-        wait
-        loop ['jump-timing']
-        play-item ['ref']
-        select ['ref']
-    endlist
-
-Stuff we probably don't need:
-
-filesystem
-    folder
-        name
-    file ['src', 'format']
-        name
-
-"""
 
 def get_xml(disc):
     """Return the vcdimager XML string for authoring a disc.
@@ -173,7 +78,6 @@ def get_xml(disc):
                 playlist.add('prev', ref='seq-title-%s' % (index - 1))
             if index < len(videos) - 1:
                 playlist.add('next', ref='seq-title-%s' % (index + 1))
-            
 
     # TODO: Proper way to include headers in libtovid.xml?
     header = '<?xml version="1.0"?>\n'
