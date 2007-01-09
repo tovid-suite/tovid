4c1337a2e7d84232f81acdc2f5c9760fa0ad76ef
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2007-01-09 02:48:35 +0000

    Multiple-titleset support

--- tovid/libtovid/author/vcdimager.py
+++ tovid/libtovid/author/vcdimager.py
@@ -7,18 +7,54 @@
 __all__ = ['get_xml']
 
 from libtovid import xml
-from libtovid import layout
+from libtovid.layout import Video, Menu, Titleset, Disc
 
+def add_titleset(titleset, ts_id, segment_items, sequence_items, pbc):
+    """Add titleset content to the XML structure."""
+    menu = titleset.menu
+    videos = titleset.videos
+    # Add menu to segment_items and pbc
+    if menu:
+        segment_items.add('segment-item', src=menu.filename,
+                          id='seg-menu-%d' % ts_id)
+        # Add menu to pbc
+        selection = pbc.add('selection', id='select-menu-%d' % ts_id)
+        selection.add('play-item', ref='seg-menu-%d' % ts_id)
+        selection.add('bsn', '1')
+        selection.add('loop', '0', jump_timing='immediate')
+        # Navigational links to titleset videos
+        for video_id in range(len(videos)):
+            selection.add('select',
+                          ref='play-title-%d-%d' % (ts_id, video_id))
+
+    # Add videos to sequence_items and pbc
+    for video_id, video in enumerate(videos):
+        sequence_item = sequence_items.add('sequence-item')
+        sequence_item.set(id='seq-title-%d-%d' % (ts_id, video_id),
+                          src=video.filename)
+        # Add chapter entries
+        for chapterid, chapter in enumerate(video.chapters):
+            sequence_item.add('entry', chapter, id='chapter-%d' % chapterid)
+
+        playlist = pbc.add('playlist')
+        playlist.set(id='play-title-%d-%d' % (ts_id, video_id))
+        playlist.add('play-item', ref='seq-title-%d-%d' % (ts_id, video_id))
+        playlist.add('wait', '0')
+        playlist.add('return', ref='select-menu')
+        # Add prev/next links if appropriate
+        if 0 <= video_id < len(videos):
+            if 0 < video_id:
+                playlist.add('prev',
+                             ref='seq-title-%d-%d' % (ts_id, video_id-1))
+            if video_id < len(videos) - 1:
+                playlist.add('next',
+                             ref='seq-title-%d-%d' % (ts_id, video_id+1))
+
+    
 def get_xml(disc):
     """Return the vcdimager XML string for authoring a disc.
-    
-        disc:  A Disc with one Titleset
     """
-    assert isinstance(disc, layout.Disc)
-    # TODO: Support more than one titleset
-    videos = disc.titlesets[0].videos
-    menu = disc.titlesets[0].menu
-    
+    assert isinstance(disc, Disc)
     # Root videocd element
     attributes = {
         'xmlns': 'http://www.gnu.org/software/vcdimager/1.0/',
@@ -27,73 +63,48 @@ def get_xml(disc):
         attributes['version'] = '2.0'
     else: # SVCD
         attributes['version'] = '1.0'
-    videocd = xml.Element('videocd')
-    videocd.set(attributes)
+    root = xml.Element('videocd')
+    root.set(attributes)
     
     # info block
-    info = videocd.add('info')
+    info = root.add('info')
     info.add('album-id', 'VIDEO_DISC')
     info.add('volume-count', '1')
     info.add('volume-number', '1')
     info.add('restriction', '0')
     
     # pvd block
-    pvd = videocd.add('pvd')
+    pvd = root.add('pvd')
     pvd.add('volume-id', 'VIDEO_DISC')
     pvd.add('system-id', 'CD-RTOS CD-BRIDGE')
     
-    # segment-items
-    if menu:
-        segment_items = videocd.add('segment-items')
-        segment_items.add('segment-item', src=menu, id='seg-menuit does')
-    
-    # sequence-items
-    sequence_items = videocd.add('sequence-items')
-    for index, video in enumerate(videos):
-        sequence_item = sequence_items.add('sequence-item')
-        sequence_item.set(id='seq-title-%d' % index, src=video.filename)
-        # Add chapter entries
-        for chapterid, chapter in enumerate(video.chapters):
-            sequence_item.add('entry', chapter, id='chapter-%d' % chapterid)
-    
-    # pbc block
-    pbc = videocd.add('pbc')
-    # Menu, if any
-    if menu:
-        selection = pbc.add('selection', id='select-menu')
-        selection.add('play-item', ref='seg-menu')
-        selection.add('bsn', '1')
-        selection.add('loop', '0', jump_timing='immediate')
-        for index in range(len(videos)):
-            selection.add('select', ref='play-title-%d' % index)
-        # TODO: Next/prev to have multi-page main menus?
-    # Video playlist items
-    for index in range(len(videos)):
-        playlist = pbc.add('playlist', id='play-title-%d' % index)
-        playlist.add('play-item', ref='seq-title-%d' % index)
-        playlist.add('wait', '0')
-        playlist.add('return', ref='select-menu')
-        if 0 <= index < len(videos):
-            if 0 < index:
-                playlist.add('prev', ref='seq-title-%s' % (index - 1))
-            if index < len(videos) - 1:
-                playlist.add('next', ref='seq-title-%s' % (index + 1))
+    segment_items = root.add('segment-items')
+    sequence_items = root.add('sequence-items')
+    pbc = root.add('pbc')
+    for ts_id, titleset in enumerate(disc.titlesets):
+        add_titleset(titleset, ts_id, segment_items, sequence_items, pbc)
 
     # TODO: Proper way to include headers in libtovid.xml?
     header = '<?xml version="1.0"?>\n'
     header += '<!DOCTYPE videocd PUBLIC "-//GNU/DTD VideoCD//EN"\n'
     header += '  "http://www.gnu.org/software/vcdimager/videocd.dtd">\n'
-    return header + str(videocd)
-
+    return header + str(root)
 
 if __name__ == '__main__':
-    videos = [
-        layout.Video('video1.mpg'),
-        layout.Video('video2.mpg'),
-        layout.Video('video3.mpg')]
-    menu = layout.Menu('menu.mpg', videos, 'vcd', 'ntsc')
-    titleset = layout.Titleset(videos, menu)
-    disc = layout.Disc('vcd_test', 'vcd', 'ntsc', [titleset])
+    videos1 = [
+        Video('video1.mpg'),
+        Video('video2.mpg'),
+        Video('video3.mpg')]
+    menu1 = Menu('menu1.mpg', videos1)
+    videos2 = [
+        Video('video4.mpg'),
+        Video('video5.mpg'),
+        Video('video6.mpg')]
+    menu2 = Menu('menu2.mpg', videos2)
+    titlesets = [
+        Titleset(videos1, menu1),
+        Titleset(videos2, menu2)]
+    disc = Disc('vcd_test', 'vcd', 'ntsc', titlesets)
 
     print "XML example"
     print get_xml(disc)
