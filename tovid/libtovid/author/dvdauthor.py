44e67013b5c300a7a921c6006d4c27eac025cfb0
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2007-01-09 02:48:59 +0000

    Multiple-titleset example

--- tovid/libtovid/author/dvdauthor.py
+++ tovid/libtovid/author/dvdauthor.py
@@ -7,12 +7,12 @@
 __all__ = ['get_xml']
 
 from libtovid import xml
-from libtovid import layout
+from libtovid.layout import Video, Menu, Titleset, Disc
 
 def get_xml(disc):
     """Return a string containing dvdauthor XML for the given Disc.
     """
-    assert isinstance(disc, layout.Disc)
+    assert isinstance(disc, Disc)
     root = xml.Element('dvdauthor', dest=disc.name.replace(' ', '_'))
     vmgm = root.add('vmgm')
     if disc.topmenu:
@@ -24,8 +24,9 @@ def get_xml(disc):
             vob.add('button', 'jump titleset %d;' % (index + 1))
 
     for titleset in disc.titlesets:
+        menu = titleset.menu
         ts = root.add('titleset')
-        if titleset.menu:
+        if menu:
             menus = ts.add('menus')
             menus.add('video')
             menus.add('pgc', entry='root')
@@ -43,13 +44,20 @@ def get_xml(disc):
 
 
 if __name__ == '__main__':
-    videos = [
-        layout.Video('video1.mpg'),
-        layout.Video('video2.mpg'),
-        layout.Video('video3.mpg')]
-    menu = layout.Menu('menu.mpg', videos, 'dvd', 'ntsc')
-    titleset = layout.Titleset(videos, menu)
-    disc = layout.Disc('dvd_test', 'dvd', 'ntsc', [titleset])
+    videos1 = [
+        Video('video1.mpg'),
+        Video('video2.mpg'),
+        Video('video3.mpg')]
+    menu1 = Menu('menu1.mpg', videos1)
+    videos2 = [
+        Video('video4.mpg'),
+        Video('video5.mpg'),
+        Video('video6.mpg')]
+    menu2 = Menu('menu2.mpg', videos2)
+    titlesets = [
+        Titleset(videos1, menu1),
+        Titleset(videos2, menu2)]
+    disc = Disc('vcd_test', 'vcd', 'ntsc', titlesets)
 
     print "XML example"
     print get_xml(disc)
