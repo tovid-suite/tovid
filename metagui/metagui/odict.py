0834695d464d4a0f4be45111109c4dec270ff55b
wapcaplet <wapcaplet@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2007-03-22 03:30:44 +0000

    Ordered dict

---
+++ metagui/metagui/odict.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python
+# odict.py
+
+"""Ordered dictionary class, from a Python Cookbook recipe:
+
+http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/107747
+
+"""
+from UserDict import UserDict
+
+class Odict (UserDict):
+    def __init__(self, keys=None, values=None):
+        """Create an Odict from the given keys and values."""
+        keys = keys or []
+        values = values or []
+        if len(keys) != len(values):
+            raise ValueError("keys and values are not the same length")
+        self._keys = keys
+        UserDict.__init__(self, dict(zip(keys, values)))
+
+    def __delitem__(self, key):
+        UserDict.__delitem__(self, key)
+        self._keys.remove(key)
+
+    def __setitem__(self, key, item):
+        UserDict.__setitem__(self, key, item)
+        if key not in self._keys: self._keys.append(key)
+
+    def clear(self):
+        UserDict.clear(self)
+        self._keys = []
+
+    def copy(self):
+        dict = UserDict.copy(self)
+        dict._keys = self._keys[:]
+        return dict
+
+    def items(self):
+        return zip(self._keys, self.values())
+
+    def keys(self):
+        return self._keys
+
+    def popitem(self):
+        try:
+            key = self._keys[-1]
+        except IndexError:
+            raise KeyError('dictionary is empty')
+
+        val = self[key]
+        del self[key]
+
+        return (key, val)
+
+    def setdefault(self, key, failobj = None):
+        UserDict.setdefault(self, key, failobj)
+        if key not in self._keys: self._keys.append(key)
+
+    def update(self, dict):
+        UserDict.update(self, dict)
+        for key in dict.keys():
+            if key not in self._keys: self._keys.append(key)
+
+    def values(self):
+        return map(self.get, self._keys)
+    
