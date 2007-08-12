b21e92a67595a303de79c6ff2d67c7b6ce615904
wapcaplet88 <wapcaplet88@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2007-08-12 19:55:17 +0000

    label and option try a new position, and like it

--- tovid/libtovid/odict.py
+++ tovid/libtovid/odict.py
@@ -71,3 +71,39 @@ class Odict (UserDict):
         for key, value in self.items():
             lines.append("%s: %s" % (key, value))
         return '\n'.join(lines)
+
+
+def convert_list(choices):
+    """Convert a list of choices to an Odict (ordered dictionary).
+    choices may be in one of several formats:
+
+               string: 'one|two|three'
+                 list: ['one', 'two', 'three']
+                 dict: {'a': "Choice A", 'b': "Choice B"}
+        list-of-lists: [['a', "Choice A"], ['b', "Choice B"], ..]
+    
+    Note: the dict form does not preserve order. Use list-of-lists
+    to maintain the specified order.
+    """
+    if type(choices) not in [str, list, dict]:
+        raise TypeError("choices must be a string, list, or dictionary.")
+
+    if type(choices) == str:
+        choices = choices.split('|')
+        return Odict(choices, choices)
+
+    if type(choices) == dict:
+        return Odict(choices.keys(), choices.values())
+
+    # choices is a list, but what kind?
+    first = choices[0]
+    # list of strings
+    if type(first) == str:
+        return Odict(choices, choices)
+    # list of 2-element string lists
+    elif type(first) == list and len(first) == 2:
+        choices, values = zip(*choices)
+        return Odict(choices, values)
+    else:
+        raise TypeError("choices lists must either be"\
+            "['a', 'b', 'c'] or [['a', 'A'], ['b', 'B']] style.")
