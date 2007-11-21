3b13e681eaf43010d5d31f5f3a6f50704324f737
wapcaplet88 <wapcaplet88@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2007-11-21 03:19:14 +0000

    List of common filetypes

---
+++ tovid/libtovid/filetypes.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# filetypes.py
+
+"""Defines several commonly-used multimedia and image file types.
+"""
+
+all_files = ('All files', '*.*')
+
+# image files
+jpeg = ('JPEG files', '*.jpg')
+png = ('PNG files', '*.png')
+tga = ('TGA files', '*.tga')
+gif = ('GIF files', '*.gif')
+bmp = ('BMP files', '*.bmp')
+image_files = ('Image files', '*.jpg *.png *.tga *.bmp *.gif')
+
+# video files
+mpeg = ('MPEG files', '*.mpg')
+avi = ('AVI files', '*.avi')
+mov = ('MOV files', '*.mov')
+mp4 = ('MPEG4 files', '*.mp4')
+rm = ('Realmedia files', '*.rm')
+mkv = ('Matroska files', '*.mkv')
+video_files = ('Video files', '*.mpg *.avi *.rm *.mkv *.mp4')
+
+# audio files
+wav = ('WAV files', '*.wav')
+mp3 = ('MP3 files', '*.mp3')
+ac3 = ('AC3 files', '*.ac3')
+mp2 = ('MP2 files', '*.mp2')
+ra = ('Realaudio files', '*.ra')
+flac = ('FLAC files', '*.flac')
+aac = ('AAC files', '*.aac')
+m4a = ('M4A files', '*.m4a')
+mpa = ('MPEG audio files', '*mpa')
+ogg = ('OGG files', '*.ogg')
+mka = ('Matroska audio files', '*.mka')
+audio_files = ('Audio files', '*.wav *.mp3 *.ac3 *.mp2 *.ra *.flac *.aac *.m4a *.mpa *.ogg *.mka')
+ 
