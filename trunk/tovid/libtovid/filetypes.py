73979d88fcf1e7f7d6ee7c20b919d0e6069efa89
wapcaplet88 <wapcaplet88@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2007-11-25 20:19:40 +0000

    Added match_types

--- trunk/tovid/libtovid/filetypes.py
+++ trunk/tovid/libtovid/filetypes.py
@@ -4,11 +4,46 @@
 """Defines several commonly-used multimedia and image file types.
 """
 
+__all__ = ['match_types']
+
 import mimetypes
 
 image_types = {}
 video_types = {}
 audio_types = {}
+
+def match_types(containing):
+    """Return a list of (type, extensions) tuples for matching mimetypes.
+    
+        containing: String or list of strings to match, for example:
+                match_types('image/jpeg')
+            Uses a "contains" search, so you can do:
+                match_types(['image', 'mpeg'])
+            to match any mimetype containing 'image' or 'mpeg'.
+
+    The returned tuples are suitable for use as the 'filetypes' argument of
+    Tkinter file dialogs (askopenfilename etc.).
+    """
+    if type(containing) == str:
+        containing = [containing]
+    elif type(containing) != list:
+        raise TypeError("match_types requires a string or list argument.")
+
+    types = {}
+    # Check for matching types and remember their extensions
+    for ext, typename in mimetypes.types_map.items():
+        for substr in containing:
+            if substr in typename:
+                # Append or insert extension
+                if typename in types:
+                    types[typename] += ' *' + ext
+                else:
+                    types[typename] = '*' + ext
+
+    # Convert to tuple-list form
+    return types.items()
+    
+    
 
 # Get all known image, video, and audio types/extensions
 for ext, typename in mimetypes.types_map.items():
