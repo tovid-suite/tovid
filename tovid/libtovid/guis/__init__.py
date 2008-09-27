ee37867d6a0489cdc9581813ca012cc3cf7f1f9f
wapcaplet88 <wapcaplet88@gmail.com>
2008-09-27 22:25:49 +0000

    Added __init__, idvid, tovid_batch

---
+++ tovid/libtovid/guis/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# __init__.py (guis)
+
+__all__ = [
+    'todisc',
+    'tovid',
+    'makemenu',
+    'makexml',
+    'idvid',
+    #'wxgtk', # for old tovid GUI, currently in libtovid.gui?
+]
