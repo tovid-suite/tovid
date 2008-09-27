b8602a1fc8942601ab45bf5ccd75d372cd7c2ea0
wapcaplet88 <wapcaplet88@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2008-09-27 22:25:49 +0000

    Added __init__, idvid, tovid_batch

---
+++ trunk/tovid/libtovid/guis/idvid.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# idvid.py
+
+from libtovid.metagui import *
+
+_terse = Flag('Terse', '-terse')
+_verbose = Flag('Verbose', '-verbose')
+_fast = Flag('Fast', '-fast')
+_tabular = Flag('Tabular', '-tabular')
+_isformat = Choice('Is format', '-isformat', 'dvd',
+    'Check the video for compliance with the given format',
+    choices='vcd|svcd|dvd',
+    toggles=True)
+_istvsys = Choice('Is TV system', '-istvsys', 'ntsc',
+    'Check whether the video is compliant with the given TV system',
+    choices='ntsc|pal',
+    toggles=True)
+
+VIDEO_FILES = List('Video files', '', None,
+    'List of multimedia video files to identify',
+    Filename())
+
+MAIN = VPanel('Main',
+    _terse,
+    _verbose,
+    _fast,
+    _tabular,
+    _isformat,
+    _istvsys,
+    VIDEO_FILES,
+)
+
+def run():
+    app = Application('idvid', MAIN)
+    gui = GUI('idvid gui', 400, 600, app)
+    gui.run()
