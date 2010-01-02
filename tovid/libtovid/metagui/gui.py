e11f8a86a1d9af2fd692c69e1f5f977c04b3f3e5
wapcaplet88 <wapcaplet88@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2010-01-02 22:12:52 +0000

    Added workaround askyesno function for Tkinter bug; changed tovid homepage to wiki

--- tovid/libtovid/metagui/gui.py
+++ tovid/libtovid/metagui/gui.py
@@ -12,7 +12,7 @@ import Tkinter as tk
 
 from widget import Widget
 from panel import Panel, Tabs
-from support import ensure_type
+from support import ensure_type, askyesno
 from libtovid import cli
 
 from ScrolledText import ScrolledText
@@ -154,7 +154,7 @@ class Executor (Widget):
 
     def notify(self, text):
         """Print a notification message to the Executor.
-        Adds newlines and angle brackets.
+        Adds newlines and brackets.
         """
         self.write("\n[[ %s ]]\n" % text)
 
@@ -288,7 +288,7 @@ class Application (Widget):
         self.tabs.activate(self.executor)
 
         # Show prompt asking whether to continue
-        if tkMessageBox.askyesno(message="Run %s now?" % self.program):
+        if askyesno(message="Run %s now?" % self.program):
             self.executor.execute(command, self.toolbar.enable)
         else:
             self.executor.notify("Cancelled.")
@@ -465,7 +465,7 @@ class GUI (tk.Tk):
     def confirm_exit(self, evt=None):
         """Exit the GUI, with confirmation prompt.
         """
-        if tkMessageBox.askyesno(message="Exit?"):
+        if askyesno(message="Exit?"):
             self.quit()
 
 
