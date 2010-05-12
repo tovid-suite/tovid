18fe566cbc5c5b59cbdb4113eb7b8b08e3f7af39
wapcaplet88 <wapcaplet88@37e97b61-3b35-0410-bc3c-4788c7a0d4e6>
2010-05-12 04:23:20 +0000

    Added uninstall capability to distutils script

--- tovid/setup.py
+++ tovid/setup.py
@@ -14,12 +14,78 @@ or:
 At this time, there is no 'uninstall' mechanism...
 """
 
-from distutils.core import setup
+import os
+import sys
+import shutil
+from distutils.core import setup, Command
+from distutils.command.install import install
+
+# Hack to get the 'uninstall' command to work
+_tovid_libdir  = os.path.join(sys.prefix, 'lib', 'tovid')
+_installed_file_log = os.path.join(_tovid_libdir, '.install.log')
+
+
+class InstallCommand (install):
+    """Overridden install command--automatically records installed files
+    to $prefix/lib/tovid/.install.log, so they can be uninstalled later.
+    """
+    def initialize_options(self):
+        install.initialize_options(self)
+        # Record installation log to a temporary file
+        self.record = 'install.log'
+
+    def run(self):
+        """Install tovid, and write a list of installed files to
+        $prefix/lib/tovid/.install.log.
+        """
+        install.run(self)
+        # Move temporary install log to $prefix/lib/tovid
+        print("Moving install.log to '%s'" % _installed_file_log)
+        shutil.move('install.log', _installed_file_log)
+
+
+class UninstallCommand (Command):
+    description = "remove everything that was installed by the install command"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Uninstall tovid, removing all files listed in
+        $prefix/lib/tovid/.install.log.
+        """
+        # If install log doesn't exist, there's nothing to do
+        if not os.path.exists(_installed_file_log):
+            print("Missing installation log: '%s'" % _installed_file_log)
+            print("Either tovid is not installed, or "
+                  "something went wrong during installation. "
+                  "You may need to uninstall manually.")
+            return
+        # Remove each file found in install log
+        for line in open(_installed_file_log, 'r'):
+            filename = line.strip(' \n')
+            print("Removing '%s'" % filename)
+            os.remove(filename)
+        # Remove the install log itself
+        os.remove(_installed_file_log)
+        # Remove the $prefix/lib/tovid directory
+        os.removedirs(_tovid_libdir)
+
+
+# The actual setup
 setup(
     name = 'tovid',
     version = 'svn',
     url = 'http://tovid.wikia.com/',
     license = 'GPL',
+    cmdclass = {
+        'install': InstallCommand,
+        'uninstall': UninstallCommand,
+    },
 
     # Python modules go into /$PREFIX/lib/pythonX.Y/(site|dist)-packages
     packages = [
