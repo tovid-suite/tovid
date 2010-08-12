852b274df09b9c31b40dade831b182703d4532fb
grepper <grepper@gmail.com>
2010-08-12 21:32:42 +0000

    Added more documentation docstrings. Use 'grab_set()' to prevent closing a GUI using VideoGui while mplayer running. Bind to self._root() for wm and keystroke exits.  Not sure how this is going to play out till it is used in a metagui Control. Added 'args' option to VideoGui for additional mplayer args. OSD menu TIME and TIME REMAINING only shown if '-menu' is included as an option to 'args'.  (osd messages on improper quit still shown). Added class SetChapters, which uses class VideoGui and will replace the 'set_chapters' python script eventually.

--- tovid/libtovid/guis/helpers.py
+++ tovid/libtovid/guis/helpers.py
@@ -4,8 +4,8 @@ import shlex
 import commands
 import re
 import os
+import sys
 from subprocess import Popen, PIPE, STDOUT
-from sys import argv
 from tempfile import mkdtemp
 
 class VideoGui(tk.Frame):
@@ -15,11 +15,29 @@ class VideoGui(tk.Frame):
     Without subclassing it only contains a 'play/pause button
     and an 'exit' button.
     """
-    def __init__(self, master, args='', title=''):
+    def __init__(self, master, args='', title=None):
+        """Initialize GUI
+
+        Arguments:
+        master: widget that will conain this GUI
+        args: additional arguments to mplayer command, inserted
+        at the end of the command just before the 'FILE' argument.
+        title: the wm title given to the master widget.
+        """
         tk.Frame.__init__(self, master)
 
         self.args = args
+        self.show_osd = False
+        if '-menu' in shlex.split(args):
+            self.show_osd = True
         self.master = master
+        if title:
+            try:
+                self.master.title(title)
+            except AttributeError:
+                print "Error: " + \
+                  "VideoGui master must be a root window for 'title' option"
+                
         self.is_running = tk.BooleanVar()
         self.is_running.set(False)
         self.pauseplay = tk.StringVar()
@@ -34,27 +52,30 @@ class VideoGui(tk.Frame):
 
     def draw(self):
         """Draw the GUI in self.master and get X11 identifier for container"""
-        self.root_frame = tk.Frame(self.master)
+        self.root_frame = tk.Frame(self)
         self.root_frame.pack(side='top', fill='both', expand=1, pady=40)
+        self.grab_set()
         self.container = tk.Frame(self.root_frame, bg='black', container=1, colormap='new')
         self.container.pack()
         # X11 identifier for the container frame
         self.xid = self.tk.call('winfo', 'id', self.container)
         # bindings for exit
-        self.master.protocol("WM_DELETE_WINDOW", self.confirm_exit)
-        self.bind('<Control-q>', self.confirm_exit)
+        self._root().protocol("WM_DELETE_WINDOW", self.confirm_exit)
+        self._root().bind('<Control-q>', self.confirm_exit)
         self.add_widgets()
 
     def add_widgets(self):
         """
         Add buttons to the VideoGui.  Override this to customize buttons.
-        This is called in draw()
+        root_frame has 'grab_set()' applied to it so make sure widgets go
+        into this frame or they will not be functional!
+        This function is called in draw()
         """
         button_frame = tk.Frame(self.root_frame)
         button_frame.pack(side='bottom', fill='x', expand=1)
         control_frame = tk.Frame(button_frame, borderwidth=1, relief='groove')
         control_frame.pack()
-        exit_button = tk.Button(control_frame, command=self.exit_mplayer, text='done !')
+        exit_button = tk.Button(control_frame, command=self.exit_mplayer, text='exit')
         pause_button = tk.Button(control_frame, command=self.pause,
                           width=12, textvariable=self.pauseplay)
         exit_button.pack(side='left')
@@ -98,8 +119,8 @@ class VideoGui(tk.Frame):
         self.set_container(video)
         command =  'mplayer -wid %s -nomouseinput -slave \
           -input nodefault-bindings:conf=/dev/null:file=%s \
-          -edlout %s %s' \
-          %(self.xid, self.cmd_pipe, self.editlist, video)
+          -edlout %s %s %s' \
+          %(self.xid, self.cmd_pipe, self.editlist, self.args, video)
         self.command = shlex.split(command)
 
     def poll(self):
@@ -122,7 +143,8 @@ class VideoGui(tk.Frame):
             if self.is_running.get():
                 cmd = Popen(self.command, stderr=open(os.devnull, 'w'), \
                   stdout=open(self.log, "w"))
-                self.send('osd 3\n')
+                if self.show_osd:
+                    self.send('osd 3\n')
                 self.pause()
         self.master.after(200, self.poll)
 
@@ -155,7 +177,8 @@ class VideoGui(tk.Frame):
             self.is_running.set(True)
             self.poll()
             # show osd time and remaining time
-            self.send('osd 3\n')
+            if self.show_osd:
+                self.send('osd 3\n')
             self.pauseplay.set('pause')
 
     def exit_mplayer(self):
@@ -172,20 +195,118 @@ class VideoGui(tk.Frame):
         time.sleep(0.3)
         self.confirm_exit()
 
-    def confirm_exit(self):
+    def confirm_exit(self, event=None):
         """on exit, make sure that mplayer is not running before quit"""
         if self.is_running.get():
-            self.send("osd_show_text 'press exit before quitting program' 4000 3\n")
+            mess = "osd_show_text 'press exit before quitting program' 4000 3\n"
+            if not self.show_osd:
+                self.send('osd 3\n%s' %mess)
+                self.after(2500, lambda:self.send('osd 0\n'))
+            else:
+                self.send(mess)
         else:
+            # run any custom commands on exit
+            self.pre_exit()
             # remove temporary files and directory
             for f in self.cmd_pipe, self.log, self.editlist:
                 if os.path.exists(f):
                     os.remove(f)
             if os.path.exists(self.tempdir):
                 os.rmdir(self.tempdir)
-            self.pre_exit()
             quit()
 
     def pre_exit(self):
         """override this function to run custom commands on exit"""
         pass
+
+
+class SetChapters(VideoGui):
+    def __init__(self, master, args='', title=None):
+        VideoGui.__init__(self, master, args, title)
+
+        self.chapter_var = tk.StringVar()
+
+    def add_widgets(self):
+        # button frame and buttons
+        button_frame = tk.Frame(self.root_frame)
+        button_frame.pack(side='bottom', fill='x', expand=1)
+        control_frame = tk.Frame(button_frame, borderwidth=1, relief='groove')
+        control_frame.pack()
+        exit_button = tk.Button(control_frame, command=self.exit_mplayer, text='done !')
+        mark_button = tk.Button(control_frame, command=self.set_chapter,text='set chapter')
+        pause_button = tk.Button(control_frame, command=self.pause,
+                          width=12, textvariable=self.pauseplay)
+        framestep_button = tk.Button(control_frame, text='step >', command=self.framestep)
+        forward_button = tk.Button(control_frame, text='seek >', command=self.forward)
+        back_button = tk.Button(control_frame, text='< seek', command=self.back)
+        # seek frame and scale widget
+        seek_frame = tk.Frame(self.root_frame)
+        seek_frame.pack(side='left', fill='x', expand=1, padx=30)
+        self.seek_scale = tk.Scale(seek_frame, from_=0, to=100, tickinterval=10,
+        orient='horizontal', label='Use slider to seek to point in file (%)')
+        self.seek_scale.bind('<ButtonRelease-1>', self.seek)
+        # pack the buttons and scale in their frames
+        mark_button.pack(side='bottom', fill='both', expand=1)
+        self.seek_scale.pack(side='left', fill='x', expand=1)
+        exit_button.pack(side='left')
+        back_button.pack(side='left')
+        pause_button.pack(side='left')
+        framestep_button.pack(side='left')
+        forward_button.pack(side='left')
+
+    def seek(self, event=None):
+        """seek in video according to value set by slider"""
+        self.send('seek %s 3\n' %self.seek_scale.get())
+
+    def forward(self):
+        """seek forward 10 seconds and make sure button var is set to 'pause'"""
+        self.send('seek 10\n')
+        self.pauseplay.set('pause')
+
+    def back(self):
+        """seek backward 10 seconds and make sure button var is set to 'pause'"""
+        self.send('seek -10\n')
+        self.pauseplay.set('pause')
+
+    def framestep(self):
+        """step frame by frame forward and set button var to 'play'"""
+        self.send('pausing frame_step\n')
+        self.pauseplay.set('play')
+
+    def set_chapter(self):
+        """send chapter mark (via slave) twice so mplayer writes the data.
+           we only take the 1st mark on each line
+        """
+        for i in range(2):
+            self.send('edl_mark\n')
+        mess = "osd_show_text 'chapter point saved' 2000 3\n"
+        if not self.show_osd:
+            self.send('osd 3\n%s' %mess)
+            self.after(2500, lambda:self.send('osd 0\n'))
+        else:
+            self.send(mess)
+
+
+    def get_chapters(self):
+        # need a sleep to make sure mplayer gives up its data
+        if not os.path.exists(self.editlist):
+            return '00:00:00'
+        time.sleep(0.5)
+        f = open(self.editlist)
+        c = f.readlines()
+        f.close()
+        # if chapter_var has value, editlist has been reset.  Append value, if any.
+        # only 1st value on each line is taken (2nd is to make mplayer write out)
+        s = [ i.split()[0]  for i  in self.chapter_var.get().splitlines() if i]
+        c.extend(s)
+        times = [ float(shlex.split(i)[0]) for i in c ]
+        chapters = ['00:00:00']
+        for t in sorted(times):
+            fraction = '.' + str(t).split('.')[1]
+            chapters.append(time.strftime('%H:%M:%S', time.gmtime(t)) + fraction)
+        return '%s' %','.join(chapters)
+
+
+    def pre_exit(self):
+        # not finished yet.  This will set the chapters variable of the Control
+        sys.stdout.write(self.get_chapters())
